1a2d08042853b6960c00c284638a627fe6d50543
vivian2 <vivian.roberts@vitality.co.uk>
2016-05-06 12:18:29 +0100

    added sitecore template items

---
+++ Vitality.Website/Areas/Presales/Models/FeatureBlocks/BenefitLeader.cs
@@ -0,0 +1,19 @@
+﻿namespace Vitality.Website.Areas.Presales.Models.FeatureBlocks
+{
+    using Glass.Mapper.Sc.Fields;
+
+    using Vitality.Website.Areas.Global.Models;
+
+    public class BenefitLeader : SitecoreItem
+    {
+        public string LeadIn { get; set; }
+
+        public string Headline { get; set; }
+
+        public string Body { get; set; }
+
+        public Link CallToAction { get; set; }
+
+        public Image BackgroundImage { get; set; }
+    }
+}
