86acced041fd6903b25785f837601218746f93cd
vivian2 <vivian.roberts@vitality.co.uk>
2016-05-06 16:59:09 +0100

    added rendering template

---
+++ Vitality.Website/Areas/Presales/RenderingModels/BenefitLeaderRendering.cs
@@ -0,0 +1,15 @@
+﻿namespace Vitality.Website.Areas.Presales.RenderingModels
+{
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+
+    using Vitality.Website.Areas.Global.Models;
+
+    [SitecoreType(TemplateId = "{B9CDB7FF-850D-4B1E-9EF0-05F29F29B16E}", AutoMap = true)]
+    public class BenefitLeaderRendering
+    {
+        [SitecoreField(Setting = SitecoreFieldSettings.DontLoadLazily)]
+        public KeyValuePair BackgroundColour { get; set; }
+        public bool BackgroundImageLeft { get; set; }
+    }
+}
---
+++ Vitality.Website/Extensions/Views/BenefitLeaderExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Vitality.Website.Extensions.Views
+{
+    using Glass.Mapper.Sc.Web.Mvc;
+
+    using Vitality.Website.Areas.Presales.Models.FeatureBlocks;
+    using Vitality.Website.Areas.Presales.RenderingModels;
+
+    public static class BenefitLeaderExtensions
+    {
+        public static string BackgroundColour(this GlassView<BenefitLeader> view)
+        {
+            if (view.GetRenderingParameters<BenefitLeaderRendering>().BackgroundColour != null)
+            {
+                return view.GetRenderingParameters<BenefitLeaderRendering>().BackgroundColour.Value;
+            }
+            return "light";
+        }
+
+        public static string BackgroundImageLeft(this GlassView<BenefitLeader> view)
+        {
+            return view.GetRenderingParameters<BenefitLeaderRendering>().BackgroundImageLeft ? "feature-block-gradient--right" : string.Empty;
+        }
+
+    }
+}
