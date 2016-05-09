54e19791eac64bdc989061753bde78feadf7c5c2
vivian2 <vivian.roberts@vitality.co.uk>
2016-05-09 13:24:54 +0100

    added sitecore integration

---
+++ Vitality.Website/Areas/Presales/Models/Rewards/RewardsLeader.cs
@@ -0,0 +1,21 @@
+﻿namespace Vitality.Website.Areas.Presales.Models.Rewards
+{
+    using System.Collections.Generic;
+
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Glass.Mapper.Sc.Fields;
+
+    using Vitality.Website.Areas.Global.Models;
+
+    public class RewardsLeader : SitecoreItem
+    {
+        public string Headline { get; set; }
+
+        public string OpeningParagraph { get; set; }
+
+        public Link CallToAction { get; set; }
+
+        [SitecoreChildren]
+        public IEnumerable<ImageLink> Rewards { get; set; }
+    }
+}
