52ed35a9186c584c8fb21a6c71850d70b0119e7d
martin m <Martin.Miles@vitality.co.uk>
2016-05-09 11:22:35 +0100

    FAQ Preview code; Gulp changed target to AfterBuild

---
+++ Vitality.Website/Areas/Presales/Models/FAQ/QuestionAnswer.cs
@@ -0,0 +1,17 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Vitality.Website.Areas.Global.Models;
+
+namespace Vitality.Website.Areas.Presales.Models.FAQ
+{
+    public class QuestionAnswer : SitecoreItem
+    {
+        [SitecoreField("Question Text")]
+        public string QuestionText { get; set; }
+
+        [SitecoreField]
+        public string Preview { get; set; }
+
+        [SitecoreField]
+        public string Answer { get; set; }
+    }
+}
