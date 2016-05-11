b09ca14f8fca41e6209d1de9e71e9e139b58c76d
martin m <Martin.Miles@vitality.co.uk>
2016-05-11 12:39:43 +0100

    PL-120: Renamed field for Glass; removed DTO annotations and hardcode; Core proj checked off build

--- Vitality.Website/Areas/Presales/Models/FAQ/FeaturedQuestions.cs
+++ Vitality.Website/Areas/Presales/Models/FAQ/FeaturedQuestions.cs
@@ -1,24 +1,19 @@
 ﻿using System.Collections.Generic;
-using Glass.Mapper.Sc.Configuration;
 using Glass.Mapper.Sc.Configuration.Attributes;
 using Glass.Mapper.Sc.Fields;
 using Vitality.Website.Areas.Global.Models;
 
 namespace Vitality.Website.Areas.Presales.Models.FAQ
 {
-    //[SitecoreType(Cachable = false)]
     public class FeaturedQuestions : SitecoreItem
     {
-        [SitecoreField]
         public string Title { get; set; }
 
-        [SitecoreField]
         public string Subtitle { get; set; }
 
         [SitecoreChildren]
         public IEnumerable<QuestionAnswer> Questions { get; set; }
 
-        [SitecoreField]
         public Link CTA { get; set; }
     }
 }
--- Vitality.Website/Areas/Presales/Models/FAQ/QuestionAnswer.cs
+++ Vitality.Website/Areas/Presales/Models/FAQ/QuestionAnswer.cs
@@ -5,13 +5,10 @@ namespace Vitality.Website.Areas.Presales.Models.FAQ
 {
     public class QuestionAnswer : SitecoreItem
     {
-        [SitecoreField("Question Text")]
         public string QuestionText { get; set; }
 
-        [SitecoreField]
         public string Preview { get; set; }
 
-        [SitecoreField]
         public string Answer { get; set; }
     }
 }
