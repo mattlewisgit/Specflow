f90f021ed47e84ab5fa69a70dd063b6efcc545db
Gavin Nicholas <gavin.nicholas@vitality.co.uk>
2016-05-11 13:35:13 +0100

    added main navigation tests

---
+++ IntegrationTests/Vitality.Website.IntegrationTests/Features/MainNavigation.feature
@@ -0,0 +1,19 @@
+﻿Feature: Main Navigation
+	In order to test that the Main Navigation works
+	As a Tester
+	I want to perform basic navigation tests
+	
+	Scenario: Test Navigation
+	Given I am on the http://dev.vitality.co.uk/
+	When I click on the business section link
+	Then I expect the http://dev.vitality.co.uk/business to open
+
+	Scenario: Click Navigation Logo
+	Given I am on the http://dev.vitality.co.uk/business
+	When I click on the navigation logo
+	Then I expect the http://dev.vitality.co.uk/ to open
+
+	Scenario: Resize home page to Mobile view and check for hamburger
+	Given I am on the http://dev.vitality.co.uk/
+	When I resize to mobile view
+	Then I expect the hamburger to be visible
---
+++ IntegrationTests/Vitality.Website.IntegrationTests/Steps/MainNavigationSteps.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Linq;
+using System.Configuration;
+using TechTalk.SpecFlow;
+using Xunit;
+using Selenium.WebDriver.Extensions.JQuery;
+using By = Selenium.WebDriver.Extensions.JQuery.By;
+
+namespace Vitality.Website.IntegrationTests
+{
+    [Binding]
+    public class MainNavigationSteps
+    {
+        private FirefoxDriver driver;
+
+        [Given(@"I am on the (.*)")]
+        public void GivenIAmOnThe(string p0)
+        {
+            var url = ConfigurationManager.AppSettings["Links.VitalityBaseUrl"] + ConfigurationManager.AppSettings[p0];
+            var ffBinary = new FirefoxBinary(@"C:\Program Files\Mozilla Firefox\Firefox.exe");
+            var firefoxProfile = new FirefoxProfile();
+
+            driver = new FirefoxDriver(ffBinary, firefoxProfile);
+            driver.Navigate().GoToUrl(p0);
+        }
+
+        [When(@"I click on the business section link")]
+        public void WhenIClickOnTheBusinessSectionLink()
+        {
+            var navItems = driver.FindElements(By.JQuerySelector(".site-nav a"));
+            var businessNavItem = navItems.FirstOrDefault(e => e.GetAttribute("href").Contains("business"));
+            businessNavItem.Click();
+        }
+        
+        [When(@"I click on the navigation logo")]
+        public void WhenIClickOnTheNavigationLogo()
+        {
+            var navItem = driver.FindElement(By.JQuerySelector(".section-nav__item--home a"));
+            navItem.Click();
+        }
+        
+        [Then(@"I expect the (.*) to open")]
+        public void ThenIExpectTheToOpen(string p0)
+        {
+            var expectedUrl = ConfigurationManager.AppSettings["Links.VitalityBaseUrl"] + ConfigurationManager.AppSettings[p0];
+            Assert.Equal(p0, driver.Url);
+        }
+
+        [When(@"I resize to mobile view")]
+        public void WhenIResizeToMobileView()
+        {
+            driver.Manage().Window.Size = new System.Drawing.Size(320, 800);
+        }
+
+        [Then(@"I expect the hamburger to be visible")]
+        public void ThenIExpectTheHamburgerToBeVisible()
+        {
+            var navItem = driver.FindElement(By.JQuerySelector(".burger-menu"));
+            Assert.NotNull(navItem);
+        }
+
+    }
+}
