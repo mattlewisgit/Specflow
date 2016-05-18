6a24a33570c0f9a99117f3b63c203e8287c08192
vivian2 <vivian.roberts@vitality.co.uk>
2016-05-18 17:14:08 +0100

    pl-219 fixed cards stacking vertically in XP editor

---
+++ UnitTests/Vitality.Website.UnitTests/Extensions/Views/RewardsLeaderExtensionsTests.cs
@@ -0,0 +1,79 @@
+﻿namespace Vitality.Website.UnitTests.Extensions.Views
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Glass.Mapper.Sc.Web.Mvc;
+
+    using Shouldly;
+
+    using Vitality.Website.Areas.Global.Models;
+    using Vitality.Website.Areas.Presales.Models.Rewards;
+    using Vitality.Website.Extensions;
+    using Vitality.Website.Extensions.Views;
+
+    using Xunit;
+
+    public class RewardsLeaderExtensionsTests
+    {
+        public class GroupedRowIconsTests
+        {
+            private readonly RewardsLeaderViewStubBuilder builder = new RewardsLeaderViewStubBuilder();
+
+            [Fact]
+            public void Model_has_no_icons_should_return_empty()
+            {
+                var view = this.builder.Build();
+
+                view.GroupedRewardsIconRows(5).ShouldBeEmpty();
+            }
+
+            [Fact]
+            public void Model_has_icons_should_return_all_icons()
+            {
+                this.builder.AddIcons(10);
+                var view = this.builder.Build();
+
+                view.GroupedRewardsIconRows(10).SelectMany(links => links).Count().ShouldBe(10);
+            }
+
+            [Fact]
+            public void Icons_per_group_should_not_exceed_icons_per_row()
+            {
+                this.builder.AddIcons(10);
+                var view = this.builder.Build();
+
+                view.GroupedRewardsIconRows(9).First().Count().ShouldBe(9);
+            }
+
+            private class RewardsLeaderViewStubBuilder
+            {
+                private readonly List<ImageLink> icons = new List<ImageLink>();
+
+                public RewardsLeaderViewStub Build()
+                {
+                    return new RewardsLeaderViewStub(new RewardsLeader { Rewards = this.icons });
+                }
+
+                public void AddIcons(int count)
+                {
+                    count.Times(() => this.icons.Add(new ImageLink()));
+                }
+            }
+
+            private class RewardsLeaderViewStub : GlassView<RewardsLeader>
+            {
+                public RewardsLeaderViewStub(RewardsLeader model)
+                {
+                    this.ViewData = new ViewDataDictionary<RewardsLeader>(model);
+                }
+
+                public override void Execute()
+                {
+                    throw new System.NotImplementedException();
+                }
+            }
+        }
+    }
+}
--- Vitality.Website/Extensions/Views/RewardsLeaderExtensions.cs
+++ Vitality.Website/Extensions/Views/RewardsLeaderExtensions.cs
@@ -1,15 +1,24 @@
 ﻿namespace Vitality.Website.Extensions.Views
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     using Glass.Mapper.Sc.Web.Mvc;
 
+    using Vitality.Website.Areas.Global.Models;
     using Vitality.Website.Areas.Presales.Models.Rewards;
     using Vitality.Website.Areas.Presales.RenderingModels;
 
     public static class RewardsLeaderExtensions
     {
-        public static string IconsPerRow(this GlassView<RewardsLeader> view)
+        public static int IconsPerRow(this GlassView<RewardsLeader> view)
         {
-            return view.GetRenderingParameters<RewardsLeaderRendering>().IconsPerRow.Value;
+            return 5.TryParseOrDefault(view.GetRenderingParameters<RewardsLeaderRendering>().IconsPerRow.Value);
+        }
+
+        public static IEnumerable<IGrouping<int, ImageLink>> GroupedRewardsIconRows(this GlassView<RewardsLeader> view, int iconsPerRow)
+        {
+            return view.Model.Rewards.Select((item, index) => new { Item = item, Index = index }).GroupBy(kv => kv.Index / iconsPerRow, kv => kv.Item);
         }
     }
 }
