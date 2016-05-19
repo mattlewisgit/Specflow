faa52247c80e49ab79fd325404b6a044ba51ec81
martin m <Martin.Miles@vitality.co.uk>
2016-05-19 15:36:10 +0100

    added images editing for EE

--- Vitality.Website/Areas/Presales/Models/FeatureBlocks/ProductHero.cs
+++ Vitality.Website/Areas/Presales/Models/FeatureBlocks/ProductHero.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Vitality.Website.Areas.Presales.Models.FeatureBlocks
+﻿namespace Vitality.Website.Areas.Presales.Models.FeatureBlocks
 {
-    public class ProductHero
+    using Glass.Mapper.Sc.Fields;
+
+    using Global.Models;
+
+    public class ProductHero : SitecoreItem
     {
+        public ProductHero()
+        {
+            this.Image = new Image();
+            this.VitalityLogo = new Image();
+        }
+
+        public string Headline { get; set; }
+
+        public string Body { get; set; }
+
+        public Link CallToAction { get; set; }
+
+        public Image Image { get; set; }
+
+        public Image VitalityLogo { get; set; }
     }
 }
