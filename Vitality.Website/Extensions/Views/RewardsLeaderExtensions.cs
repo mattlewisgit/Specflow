799b0f49c540df099117de19db271b46fb664585
vivian2 <vivian.roberts@vitality.co.uk>
2016-05-19 10:49:55 +0100

    moved row grouping into shared extension method

--- Vitality.Website/Extensions/Views/RewardsLeaderExtensions.cs
+++ Vitality.Website/Extensions/Views/RewardsLeaderExtensions.cs
@@ -1,11 +1,7 @@
 ﻿namespace Vitality.Website.Extensions.Views
 {
-    using System.Collections.Generic;
-    using System.Linq;
-
     using Glass.Mapper.Sc.Web.Mvc;
 
-    using Vitality.Website.Areas.Global.Models;
     using Vitality.Website.Areas.Presales.Models.Rewards;
     using Vitality.Website.Areas.Presales.RenderingModels;
 
@@ -14,11 +10,6 @@
         public static int IconsPerRow(this GlassView<RewardsLeader> view)
         {
             return 5.TryParseOrDefault(view.GetRenderingParameters<RewardsLeaderRendering>().IconsPerRow.Value);
-        }
-
-        public static IEnumerable<IGrouping<int, ImageLink>> GroupedRewardsIconRows(this GlassView<RewardsLeader> view, int iconsPerRow)
-        {
-            return view.Model.Rewards.Select((item, index) => new { Item = item, Index = index }).GroupBy(kv => kv.Index / iconsPerRow, kv => kv.Item);
         }
     }
 }
