e202f07c38b0985f860f5f29dd41d977963dbfde
vivian2 <vivian.roberts@vitality.co.uk>
2016-05-23 14:14:22 +0100

    added optional logic for lead in and opening paragraph

---
+++ Vitality.Website/Extensions/Views/SummaryComponentExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Vitality.Website.Extensions.Views
+{
+    using Glass.Mapper.Sc.Web.Mvc;
+
+    using Vitality.Website.Areas.Presales.Models.Summary;
+
+    public static class SummaryComponentExtensions
+    {
+        public static bool CanShowLeadIn(this GlassView<SummaryComponent> view)
+        {
+            return view.IsInEditingMode || !string.IsNullOrWhiteSpace(view.Model.LeftContentLeadIn);
+        }
+
+        public static bool CanShowOpeningParagraph(this GlassView<SummaryComponent> view)
+        {
+            return view.IsInEditingMode || !string.IsNullOrWhiteSpace(view.Model.LeftContentOpeningParagraph);
+        }
+    }
+}
