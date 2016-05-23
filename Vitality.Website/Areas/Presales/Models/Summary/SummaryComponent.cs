e852e011f2a576af007938ec5e71aaca15f0f0e0
vivian2 <vivian.roberts@vitality.co.uk>
2016-05-23 11:46:45 +0100

    added left half model

--- Vitality.Website/Areas/Presales/Models/Summary/SummaryComponent.cs
+++ Vitality.Website/Areas/Presales/Models/Summary/SummaryComponent.cs
@@ -4,5 +4,12 @@
 
     public class SummaryComponent : SitecoreItem
     {
+        public string LeftContentLeadIn { get; set; }
+
+        public string LeftContentHeadline { get; set; }
+
+        public string LeftContentOpeningParagraph { get; set; }
+
+        public string LeftContentBody { get; set; }
     }
 }
