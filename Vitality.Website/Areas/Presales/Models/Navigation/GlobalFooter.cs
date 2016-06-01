507e0fa4ab3b4358e5e34ff7d99cad36dce371e1
vivian2 <vivian.roberts@vitality.co.uk>
2016-06-01 17:54:15 +0100

    added extension methods

--- Vitality.Website/Areas/Presales/Models/Navigation/GlobalFooter.cs
+++ Vitality.Website/Areas/Presales/Models/Navigation/GlobalFooter.cs
@@ -4,7 +4,6 @@ using Vitality.Website.Areas.Global.Models;
 namespace Vitality.Website.Areas.Presales.Models.Navigation
 {
     using System.Collections.Generic;
-    using System.Linq;
 
     using Glass.Mapper.Sc.Configuration;
     using Glass.Mapper.Sc.Configuration.Attributes;
@@ -23,19 +22,6 @@ namespace Vitality.Website.Areas.Presales.Models.Navigation
 
         [SitecoreField(Setting = SitecoreFieldSettings.DontLoadLazily)]
         public IEnumerable<FooterSection> FooterLinksColumn3 { get; set; }
-
-        [SitecoreIgnore]
-        public IEnumerable<FooterSection> FooterLinksAllColumns
-        {
-            get
-            {
-                var allcolumns = new List<FooterSection>();
-                allcolumns.AddRange(this.FooterLinksColumn1);
-                allcolumns.AddRange(this.FooterLinksColumn2);
-                allcolumns.AddRange(this.FooterLinksColumn3);
-                return allcolumns;
-            }
-        }
 
         [SitecoreField(Setting = SitecoreFieldSettings.DontLoadLazily)]
         public IEnumerable<FooterSocialSection> FooterSocialLinksColumn { get; set; }
