0d143a15793dd42e076558ef1f637586d4e01df5
vivian2 <vivian.roberts@vitality.co.uk>
2016-06-01 17:34:12 +0100

    added sitecore items and cshtml

---
+++ Vitality.Website/Areas/Presales/Models/Navigation/GlobalFooter.cs
@@ -0,0 +1,43 @@
+﻿using Glass.Mapper.Sc.Fields;
+using Vitality.Website.Areas.Global.Models;
+
+namespace Vitality.Website.Areas.Presales.Models.Navigation
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+
+    public class GlobalFooter : SitecoreItem
+    {
+        public Image Logo { get; set; }
+
+        public string CopyrightStatement { get; set; }
+
+        [SitecoreField(Setting = SitecoreFieldSettings.DontLoadLazily)]
+        public IEnumerable<FooterSection> FooterLinksColumn1 { get; set; }
+
+        [SitecoreField(Setting = SitecoreFieldSettings.DontLoadLazily)]
+        public IEnumerable<FooterSection> FooterLinksColumn2 { get; set; }
+
+        [SitecoreField(Setting = SitecoreFieldSettings.DontLoadLazily)]
+        public IEnumerable<FooterSection> FooterLinksColumn3 { get; set; }
+
+        [SitecoreIgnore]
+        public IEnumerable<FooterSection> FooterLinksAllColumns
+        {
+            get
+            {
+                var allcolumns = new List<FooterSection>();
+                allcolumns.AddRange(this.FooterLinksColumn1);
+                allcolumns.AddRange(this.FooterLinksColumn2);
+                allcolumns.AddRange(this.FooterLinksColumn3);
+                return allcolumns;
+            }
+        }
+
+        [SitecoreField(Setting = SitecoreFieldSettings.DontLoadLazily)]
+        public IEnumerable<FooterSocialSection> FooterSocialLinksColumn { get; set; }
+    }
+}
