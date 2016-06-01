fa872eb5bf6e34f7f39d92d74790de7f4f24fa29
David Buckell <david.buckell@vitality.co.uk>
2016-06-01 11:12:56 +0100

    added content component

---
+++ Vitality.Website/Areas/Presales/Models/Generic/Content.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vitality.Website.Areas.Global.Models;
+
+namespace Vitality.Website.Areas.Presales.Models.Generic
+{
+    public class Content : SitecoreItem
+    {
+        public Content()
+        {
+            
+        }
+
+        public string RichText { get; set; }
+    }
+}
