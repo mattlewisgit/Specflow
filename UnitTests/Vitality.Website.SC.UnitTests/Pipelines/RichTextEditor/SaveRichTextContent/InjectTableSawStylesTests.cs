ac72658c27bc42b302faafbd40906b7b0e7b8410
David Buckell <david.buckell@vitality.co.uk>
2016-06-03 17:01:36 +0100

    hide non-required tabs from table wizard & added pipeline to inject tablesaw markup into RTE tables

---
+++ UnitTests/Vitality.Website.SC.UnitTests/Pipelines/RichTextEditor/SaveRichTextContent/InjectTableSawStylesTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Shouldly;
+using Sitecore.Shell.Controls.RichTextEditor.Pipelines.SaveRichTextContent;
+using Vitality.Website.SC.Pipelines.RichTextEditor.SaveRichTextContent;
+using Xunit;
+
+namespace Vitality.Website.SC.UnitTests.Pipelines.RichTextEditor.SaveRichTextContent
+{
+    public class InjectTableSawStylesTests
+    {
+        private readonly InjectTableSawStyles injectTableStyles;
+        private readonly SaveRichTextContentArgs args;
+
+        public InjectTableSawStylesTests()
+        {
+            var content = "<p>some test content <table><tr></tr></table><br><table><tr></tr></table></p>";
+            this.args = new SaveRichTextContentArgs(content);
+            this.injectTableStyles = new InjectTableSawStyles();
+        }
+
+        [Fact]
+        public void Content_Should_Not_Be_Modified_If_Content_Does_Not_Contain_Tables()
+        {
+            var content = "<p>some table test content</p>";
+            args.Content = content;
+            
+            injectTableStyles.Process(args);
+
+            args.Content.ShouldBe(content);
+        }
+
+        [Fact]
+        public void Should_inject_table_saw_class_when_table_element_present()
+        {
+            var expected = "class=\"tablesaw tablesaw-stack\"";
+            
+            injectTableStyles.Process(args);
+
+            args.Content.ShouldContain(expected);
+        }
+
+        [Fact]
+        public void Should_inject_table_saw_data_attribute_when_table_element_present()
+        {
+            var expected = "data-tablesaw-mode=\"stack\"";
+
+            injectTableStyles.Process(args);
+
+            args.Content.ShouldContain(expected);
+        }
+
+        [Fact]
+        public void Should_not_modify_content_if_table_already_contains_tablesaw_class()
+        {
+            var expected = "<p>some test content <table class=\"tablesaw tablesaw-stack\" data-tablesaw-mode=\"stack\"><tr></tr></table></p>";
+            args.Content = expected;
+            var index = expected.LastIndexOf("class=\"tablesaw tablesaw-stack\"", StringComparison.OrdinalIgnoreCase);
+            
+            injectTableStyles.Process(args);
+
+            args.Content.LastIndexOf("class=\"tablesaw tablesaw-stack\"", StringComparison.OrdinalIgnoreCase).ShouldBe(index);
+        }
+
+        [Fact]
+        public void Should_not_modify_content_if_table_already_contains_tablesaw_data_attribute()
+        {
+            var expected = "<p>some test content <table class=\"tablesaw tablesaw-stack\" data-tablesaw-mode=\"stack\"><tr></tr></table></p>";
+            args.Content = expected;
+            var index = expected.LastIndexOf("data-tablesaw-mode=\"stack\"", StringComparison.OrdinalIgnoreCase);
+
+            injectTableStyles.Process(args);
+
+            args.Content.LastIndexOf("data-tablesaw-mode=\"stack\"", StringComparison.OrdinalIgnoreCase).ShouldBe(index);
+        }
+
+        [Fact]
+        public void Should_inject_table_saw_class_and_data_attribute_for_all_tables()
+        {
+            var expected = "<p>some test content <table class=\"tablesaw tablesaw-stack\" data-tablesaw-mode=\"stack\"><tr></tr></table>" +
+                           "<br><table class=\"tablesaw tablesaw-stack\" data-tablesaw-mode=\"stack\"><tr></tr></table></p>";
+
+            injectTableStyles.Process(args);
+
+            args.Content.ShouldBe(expected);
+        }
+    }
+}
---
+++ Vitality.Website.SC/Pipelines/RichTextEditor/SaveRichTextContent/InjectTableSawStyles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Web.UI;
+using Sitecore.Shell.Controls.RichTextEditor.Pipelines.SaveRichTextContent;
+using Sitecore.WordOCX.HtmlDocument;
+
+namespace Vitality.Website.SC.Pipelines.RichTextEditor.SaveRichTextContent
+{
+    public class InjectTableSawStyles
+    {
+        public void Process(SaveRichTextContentArgs args)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(args.Content);
+
+            var tables = htmlDoc.DocumentNode.SelectNodes("//table");
+
+            if (tables != null)
+            {
+                foreach (var table in tables)
+                {
+                    if (table.Attributes["class"] == null)
+                    {
+                        table.Attributes.Add("class", string.Empty);
+                    }
+                    if (table.Attributes["data-tablesaw-mode"] == null)
+                    {
+                        table.Attributes.Add("data-tablesaw-mode", string.Empty);
+                    }
+
+                    table.Attributes["class"].Value = "tablesaw tablesaw-stack";
+                    table.Attributes["data-tablesaw-mode"].Value = "stack";
+                }
+            }
+
+            using (var textWriter = new StringWriter())
+            {
+                htmlDoc.Save(textWriter);
+                args.Content = textWriter.ToString();
+            }
+        }
+    }
+}
+    
