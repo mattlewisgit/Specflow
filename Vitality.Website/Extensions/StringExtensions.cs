645162e1b513669508c5cc1f151cabb448142c16
vivian2 <vivian.roberts@vitality.co.uk>
2016-06-03 12:24:50 +0100

    fixed non-hypenated id names

--- Vitality.Website/Extensions/StringExtensions.cs
+++ Vitality.Website/Extensions/StringExtensions.cs
@@ -1,10 +1,17 @@
 ﻿namespace Vitality.Website.Extensions
 {
+    using Vitality.Website.SC.Utilities;
+
     public static class StringExtensions
     {
         public static string FormatWith(this string @string, params object[] values)
         {
             return string.Format(@string, values);
+        }
+
+        public static string ToLowerHypenatedString(this string @string)
+        {
+            return StringHelper.HyphenatedWords(@string).ToLower();
         }
     }
 }
