3398ceaf98027080410aafabdf3eeb6f3ca810b2
vivian2 <vivian.roberts@vitality.co.uk>
2016-06-14 17:14:40 +0100

    added quote footer items and cshtml

--- Vitality.Website.SC/ItemConstants.cs
+++ Vitality.Website.SC/ItemConstants.cs
@@ -10,6 +10,7 @@
             {
                 public static class Home
                 {
+                    public static readonly Guid Id = Guid.Parse("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}");
                     public const string Path = "/sitecore/content/Presales/home";
                 }
 
