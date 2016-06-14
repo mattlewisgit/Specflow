2c8b0897855de20ef547d3f6042a8b57eb1a62c1
vivian2 <vivian.roberts@vitality.co.uk>
2016-06-14 17:42:15 +0100

    additional items and map headline

--- Vitality.Website/Areas/Presales/BaseTemplates/IQuoteFooter.cs
+++ Vitality.Website/Areas/Presales/BaseTemplates/IQuoteFooter.cs
@@ -1,19 +1,36 @@
 ﻿namespace Vitality.Website.Areas.Presales.BaseTemplates
 {
+    using System;
     using System.Collections.Generic;
 
     using Glass.Mapper.Sc.Configuration;
     using Glass.Mapper.Sc.Configuration.Attributes;
+    using Glass.Mapper.Sc.Maps;
 
     using Vitality.Website.Areas.Global.Models;
+    using Vitality.Website.Areas.Presales.SettingsTemplates;
 
     public interface IQuoteFooter
     {
+        string Headline { get; set; }
+
         bool InheritQuoteFooterSettings { get; set; }
 
         bool ShowQuoteFooter { get; set; }
 
         [SitecoreField(Setting = SitecoreFieldSettings.DontLoadLazily)]
         IEnumerable<LinkItem> QuoteFooterLinks { get; set; }
+    }
+
+    public class QuoteFooterConfig : SitecoreGlassMap<IQuoteFooter>
+    {
+        public override void Configure()
+        {
+            this.Map(
+                x => x.AutoMap(),
+                x => x.Delegate(footer => footer.Headline).GetValue(
+                    context => context.Service.GetItem<QuoteFooter>(Guid.Parse("{5F2E1820-3907-400D-B0F4-3C8D6DDB0989}")).Headline)
+                );
+        }
     }
 }
