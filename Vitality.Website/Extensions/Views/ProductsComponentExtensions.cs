3b5f04da7f5b4d6dc4ac1ec8876dfa59aa0b285b
Damith Gunwardana <damith.gunawardana@vitality.co.uk>
2016-06-16 12:24:55 +0100

    Override img for product components and uncommented gulp targets

---
+++ Vitality.Website/Extensions/Views/ProductsComponentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glass.Mapper.Sc.Web.Mvc;
+using Vitality.Website.Areas.Presales.Models.Products;
+
+namespace Vitality.Website.Extensions.Views
+{
+    public static class ProductsComponentExtensions
+    {
+
+        public static int NumberOfColumns(this GlassView<ProductsComponent> view)
+        {
+            return view.Model.ProductPanels.Count();
+        }
+    }
+}
