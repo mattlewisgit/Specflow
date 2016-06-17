81f1820dd1c0c5cbf71aae907f45b6bd4432aaeb
Damith Gunwardana <damith.gunawardana@vitality.co.uk>
2016-06-17 17:17:28 +0100

    Partner hero pl-109 boiler plates and partial site core templates

---
+++ Vitality.Website/Areas/Presales/Models/Partners/PartnerHero.cs
@@ -0,0 +1,30 @@
+﻿using Glass.Mapper.Sc.Fields;
+using Vitality.Website.Areas.Global.Models;
+
+namespace Vitality.Website.Areas.Presales.Models.Partners
+{
+    public class PartnerHero : SitecoreItem
+    {
+        public PartnerHero()
+        {
+            this.PartnerIcon = new Image();
+            this.Image = new Image();
+        }
+
+        public Image Image { get; set; }
+        public string Headline { get; set; }
+
+        public string OpeningParagraph { get; set; }
+
+        public Image PartnerIcon { get; set; }
+
+        public string PartnerName { get; set; }
+
+        public string Line { get; set; }
+
+        public string Benefits { get; set; }
+    }
+}
+
+
+
