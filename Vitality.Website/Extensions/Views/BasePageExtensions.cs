1150b4772d3ee31424f39e0b7ed71aca78a177a0
vivian2 <vivian.roberts@vitality.co.uk>
2016-06-17 12:20:45 +0100

    home page logic and absolute urls

---
+++ Vitality.Website/Extensions/Views/BasePageExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Vitality.Website.Extensions.Views
+{
+    using System;
+
+    using Glass.Mapper.Sc.Web.Mvc;
+
+    using Vitality.Website.Areas.Presales.PageTemplates;
+    using Vitality.Website.SC;
+
+    public static class BasePageExtensions
+    {
+        //TODO: Consider removing once SEO requirements come in and absolute Urls are managed by Sitecore
+        public static string AbsoluteUrl(this GlassView<BasePage> view, string relative)
+        {
+            return "https://" + Sitecore.Context.Site.HostName + relative;
+        }
+
+        public static bool IsHomePage(this GlassView<BasePage> view)
+        {
+            return Sitecore.Context.Item.Paths.FullPath.Equals(ItemConstants.Presales.Content.Home.Path, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
