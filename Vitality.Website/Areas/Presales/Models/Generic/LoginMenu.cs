1de93c6829cc58890223c809e67cbbdd1c5f4fce
Damith Gunwardana <damith.gunawardana@vitality.co.uk>
2016-06-21 15:20:59 +0100

    Member Login Sitecore template added

---
+++ Vitality.Website/Areas/Presales/Models/Generic/LoginMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glass.Mapper.Sc.Fields;
+using Vitality.Website.Areas.Global.Models;
+
+namespace Vitality.Website.Areas.Presales.Models.Generic
+{
+    public class LoginMenu : SitecoreItem
+    {
+        public Link MemberLink { get; set; }
+        public Link LifeAdviserLink { get; set; }
+        public Link HealthAdviserLink { get; set; }
+        public Link EmployerLink { get; set; }
+        public Link HealthcarePractitionerLink { get; set; }
+    }
+}
