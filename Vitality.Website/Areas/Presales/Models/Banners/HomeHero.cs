9b1b008d486868bb818b6dcaaead8c2b0391645f
vivian2 <vivian.roberts@vitality.co.uk>
2016-06-21 13:25:32 +0100

    added sitecore items and cshtml

---
+++ Vitality.Website/Areas/Presales/Models/Banners/HomeHero.cs
@@ -0,0 +1,22 @@
+﻿namespace Vitality.Website.Areas.Presales.Models.Banners
+{
+    using Glass.Mapper.Sc.Fields;
+
+    using Vitality.Website.Areas.Global.Models;
+
+    public class HomeHero : SitecoreItem
+    {
+        public HomeHero()
+        {
+            this.PosterImage = new Image();
+        }
+
+        public Image PosterImage { get; set; }
+
+        public string Headline { get; set; }
+
+        public string SecondHeadline { get; set; }
+
+        public string OpeningParagraph { get; set; }
+    }
+}
---
+++ Vitality.Website/Extensions/Views/HomeHeroExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Vitality.Website.Extensions.Views
+{
+    using Glass.Mapper.Sc.Web.Mvc;
+
+    using Vitality.Website.Areas.Presales.Models.Banners;
+
+    public static class HomeHeroExtensions
+    {
+        public static string BackgroundImage(this GlassView<HomeHero> view)
+        {
+            return string.Format("background-image: url('{0}')", view.Model.PosterImage.Src);
+        }
+    }
+}
