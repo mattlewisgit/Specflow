493572f90c621e634c9141aba71cc073f975327e
vivian2 <vivian.roberts@vitality.co.uk>
2016-06-28 12:25:33 +0100

    fixed content component

--- Vitality.Website/Areas/Presales/ComponentTemplates/Generic/Content.cs
+++ Vitality.Website/Areas/Presales/ComponentTemplates/Generic/Content.cs
@@ -1,7 +1,7 @@
-﻿using Vitality.Website.Areas.Global.Models;
-
-namespace Vitality.Website.Areas.Presales.Models.Generic
+﻿namespace Vitality.Website.Areas.Presales.ComponentTemplates.Generic
 {
+    using Vitality.Website.Areas.Global.Models;
+
     public class Content : SitecoreItem
     {
         public string RichText { get; set; }
