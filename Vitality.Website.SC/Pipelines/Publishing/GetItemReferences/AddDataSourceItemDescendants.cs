24204969a2e1130a6beb5b5be8f6d13a018cd131
David Buckell <david.buckell@vitality.co.uk>
2016-07-05 17:26:00 +0100

    include items referrers in publishing queue

--- Vitality.Website.SC/Pipelines/Publishing/GetItemReferences/AddDataSourceItemDescendants.cs
+++ Vitality.Website.SC/Pipelines/Publishing/GetItemReferences/AddDataSourceItemDescendants.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using Sitecore;
 using Sitecore.Data.Items;
+using Sitecore.Links;
 using Sitecore.Publishing.Pipelines.GetItemReferences;
 using Sitecore.Publishing.Pipelines.PublishItem;
 
@@ -17,26 +20,38 @@ namespace Vitality.Website.SC.Pipelines.Publishing.GetItemReferences
 
         protected override List<Item> GetItemReferences(PublishItemContext context)
         {
+            // Get context items Related Items
             var referredItem = context.Result.ReferredItems.Select(pc => context.PublishHelper.GetSourceItem(pc.ItemId)).ToArray();
 
             var itemsToPublish = new List<Item>();
-            foreach (var item in referredItem)
+            // Iterate over Related Items that are datasource items
+            foreach (var item in referredItem.Where(ItemIsDataSource))
             {
-                if (ItemIsDataSource(item))
+                // Add datasource items to publishing queue, if not already present (includes the items referrer items)
+                if (!referredItem.Contains(item) && !itemsToPublish.Contains(item))
                 {
-                    foreach (Item child in item.Children)
+                    itemsToPublish.AddRange(GetLinkedItems(item));
+                }
+
+                // Add Related Items child items to publishing queue, if not already present (includes the items referrer items)
+                foreach (Item child in item.Children)
+                {
+                    if (!referredItem.Contains(child) && !itemsToPublish.Contains(child))
                     {
-                        if (!referredItem.Contains(child) && 
-                            !itemsToPublish.Contains(child))
-                        {
-                            itemsToPublish.Add(child);
-                        }
+                        itemsToPublish.AddRange(GetLinkedItems(child));
                     }
-                    
                 }
             }
 
             return itemsToPublish;
+        }
+
+        private IEnumerable<Item> GetLinkedItems(Item contextItem)
+        {
+            // Retrieve the items referrer items
+            var links = contextItem.Links.GetValidLinks();
+            // Return referrer items which are datasource items
+            return links.Select(link => contextItem.Database.GetItem(link.TargetItemID)).Where(targetItem => targetItem != null && ItemIsDataSource(targetItem));
         }
 
         private bool ItemIsDataSource(Item item)
