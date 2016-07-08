416e5c09f788e736c905f732500d89ef14624346
Darren Hickling <darren.hickling@vitality.co.uk>
2016-07-08 10:58:57 +0100

    Using JSON example from Viv and conditionally adding current list item state

--- Boilerplate/src/js/angular/healthAdvisersLiteratureApp.js
+++ Boilerplate/src/js/angular/healthAdvisersLiteratureApp.js
@@ -1,43 +1,35 @@
 ﻿angular
     .module("HealthAdvisersSalesLiteratureApp", [])
-    .controller("ChooseLiteratureController", function ($scope) {
+    .controller("ChooseController", ["$scope", "$window", function($scope, $window) {
         "use strict";
-        $scope.types = [
-            {
-                name: "Vitality GP"
-            },
-            {
-                name: "Vitality Something"
-            }
-        ];
-    })
-    .controller("AvailableLiteratureController", function ($scope) {
+        $scope.types = $window.AvailableLiteratureTypes;
+    }])
+    .controller("AvailableController", function ($scope) {
         "use strict";
         $scope.literature = [
-            {
-                name: "Health and rewards partners"
-            },
-            {
-                name: "Vitality GP Select"
-            },
-            {
-                name: "Vitality GP"
-            },
-            {
-                name: "Hospital list"
-            },
-            {
-                name: "Discovery company profile"
-            }
+        {
+            "Key": "guide-to-cover---business-healthcare-2015",
+            "CategoryKey": "business-and-corporate-healthcare-literature",
+            "Title": "Guide to Cover - Business Healthcare 2015",
+            "IsSelected": true
+        },
+        {
+            "Key": "guide-to-cover---personal-healthcare-2015",
+            "CategoryKey": "personal-healthcare-literature",
+            "Title": "Guide to Cover - Personal Healthcare 2015"
+        }
         ];
     })
-    .controller("LiteratureDocumentController", function ($scope) {
+    .controller("DocumentController", function ($scope) {
         "use strict";
         $scope.document = {
-            code: "CODE",
-            date: "12 July 2016",
-            description: "Lorem ipsum descriptivio",
-            imageSource: "/src/img/examples/example-pdf.png",
-            name: "Healthy Living Rewards and Partners from VitalityHealth"
+            "Title": "Personal Healthcare sales aid",
+            "Description": "Personal Healthcare with VitalityHealth",
+            "PublishDate": "2016-07-06T23:00:00Z",
+            "Document": "dev.vitality.co.uk/-/media/Presales/pdf/Development/PHF-sales-aid-v2.ashx",
+            "Thumbnail": "/src/img/examples/example-pdf.png",
+            "Code": "PHU1294A",
+            "Category": "Personal healthcare literature",
+            "AvailableLiterature": []
         };
     });
