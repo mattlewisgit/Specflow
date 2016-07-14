60abe54d700b23b31c3314da256eb667cee8e2a6
Darren Hickling <darren.hickling@vitality.co.uk>
2016-07-14 17:39:21 +0100

    PL-586 Create Boilerplate Animating and hiding based on JavaScript rules Just need to add the cookie and update the examples

---
+++ Boilerplate/src/js/modules/cookieMessageModule.js
@@ -0,0 +1,35 @@
+﻿var _container = null;
+var _cta = null;
+
+function init() {
+    "use strict";
+    // Find the elements or bomb out if they are not present.
+    _container = $(".cookie-message");
+
+    if (_container.length < 1) {
+        return;
+    }
+
+    _cta = _container.find(".box-button");
+
+    // TODO Determine whether to continue based on a cookie.
+    _container.show();
+
+    // Hide and set the cookie on close.
+    _cta.click(function() {
+        _container
+            .removeClass("slideInDown")
+            .addClass("slideOutUp");
+
+        setTimeout(function () {
+            // TODO Set the cookie.
+            _container.hide();
+        }, 500);
+
+        return false;
+    });
+}
+
+module.exports = {
+    init: init
+};
