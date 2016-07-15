665f29e6524a868ab43e9ec47915cbcbab891633
David Buckell <david.buckell@vitality.co.uk>
2016-07-15 17:20:07 +0100

    applied persist class

--- Vitality.Website.SC/Pipelines/RenderField/TableSawHelper.cs
+++ Vitality.Website.SC/Pipelines/RenderField/TableSawHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Linq;
+using Sitecore.Security.AccessControl;
 using Sitecore.WordOCX.HtmlDocument;
 
 namespace Vitality.Website.SC.Pipelines.RenderField
@@ -8,9 +9,13 @@ namespace Vitality.Website.SC.Pipelines.RenderField
     {
         public const string Class = "class";
         public const string DataTablesawMode = "data-tablesaw-mode";
+        public const string DataTablesawMinimap = "data-tablesaw-minimap";
+        public const string DataTablesawPriority = "data-tablesaw-priority";
 
         public static string AddTableAttributes(TableSawArgs args)
         {
+            bool isSwipe = args.TableMode.ToLowerInvariant() == "swipe";
+
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(args.Html);
 
@@ -30,10 +35,31 @@ namespace Vitality.Website.SC.Pipelines.RenderField
                         attributes.Add(DataTablesawMode, string.Empty);
                     }
 
+                    if (attributes[DataTablesawMinimap] == null && isSwipe)
+                    {
+                        attributes.Add(DataTablesawMinimap, string.Empty);
+                    }
+                    
                     attributes[Class].Value = args.TableClass;
                     attributes[DataTablesawMode].Value = args.TableMode;
-                }
+                }                
+            }
 
+            if (isSwipe)
+            {
+                var tableHeaders = htmlDoc.DocumentNode.SelectNodes("//table//thead//tr/*[1]");
+
+                if (tableHeaders != null)
+                {
+                    foreach (var tableHeader in tableHeaders)
+                    {
+                        tableHeader.Attributes.Add(DataTablesawPriority, "persist");
+                    }
+                }
+            }
+
+            if (tables != null || isSwipe)
+            {
                 using (var textWriter = new StringWriter())
                 {
                     htmlDoc.Save(textWriter);
