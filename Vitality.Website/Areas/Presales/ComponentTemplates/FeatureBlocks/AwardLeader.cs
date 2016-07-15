ac5a3c899f3ab6731e5a1ec65d7ffe720fe71ca6
Damith Gunwardana <damith.gunawardana@vitality.co.uk>
2016-07-15 15:04:35 +0100

    partial changes

---
+++ Vitality.Website/Areas/Presales/ComponentTemplates/FeatureBlocks/AwardLeader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glass.Mapper.Sc.Fields;
+using Vitality.Website.Areas.Global.Models;
+
+namespace Vitality.Website.Areas.Presales.ComponentTemplates.FeatureBlocks
+{
+    public class AwardLeader : SitecoreItem
+    {
+        public AwardLeader()
+        {
+            this.BackgroundImage = new Image();
+        }
+
+        public string LeadIn { get; set; }
+
+        public string Headline { get; set; }
+
+        public string OpeningParagraph { get; set; }
+
+        public Image BackgroundImage { get; set; }
+    }
+}
