c55f0f458beda51d2ca4e88761f37bdeaa8328d1
vivian2 <vivian.roberts@vitality.co.uk>
2016-07-18 13:18:17 +0100

    added view

--- Vitality.Website/Areas/Presales/SettingsTemplates/CookieSettings.cs
+++ Vitality.Website/Areas/Presales/SettingsTemplates/CookieSettings.cs
@@ -4,13 +4,11 @@
 
     using Vitality.Website.Areas.Global.Models;
 
-    [SitecoreType(Cachable = true)]
+    [SitecoreType(Cachable = true, AutoMap = true)]
     public class CookieSettings : SitecoreItem
     {
         public string Message { get; set; }
 
         public string AcceptText { get; set; }
-
-        public int ExpirationInDays { get; set; }
     }
 }
---
+++ Vitality.Website/Extensions/Views/CookieMessageExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Vitality.Website.Extensions.Views
+{
+    using Glass.Mapper.Sc.Web.Mvc;
+
+    using Vitality.Website.Areas.Presales.SettingsTemplates;
+    using Vitality.Website.SC.Pipelines.HttpRequest;
+
+    public static class CookieMessageExtensions
+    {
+        public static bool ShowCookieMessage(this GlassView<CookieSettings> view)
+        {
+            var cookieMessageCookie = view.Request.Cookies.Get(CookieMessage.Name);
+            if (cookieMessageCookie != null)
+            {
+                return cookieMessageCookie.Value == CookieMessage.Show;
+            }
+            return true;
+        }
+    }
+}
