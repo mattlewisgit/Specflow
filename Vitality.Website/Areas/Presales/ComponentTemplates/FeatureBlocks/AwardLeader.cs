2878fd5324416e6f6c84035c12cc3802932ca3e9
Damith Gunwardana <damith.gunawardana@vitality.co.uk>
2016-07-19 11:23:56 +0100

    Partial update to award leader sitecore template.

--- Vitality.Website/Areas/Presales/ComponentTemplates/FeatureBlocks/AwardLeader.cs
+++ Vitality.Website/Areas/Presales/ComponentTemplates/FeatureBlocks/AwardLeader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Glass.Mapper.Sc.Configuration.Attributes;
 using Glass.Mapper.Sc.Fields;
 using Vitality.Website.Areas.Global.Models;
 
@@ -21,5 +22,39 @@ namespace Vitality.Website.Areas.Presales.ComponentTemplates.FeatureBlocks
         public string OpeningParagraph { get; set; }
 
         public Image BackgroundImage { get; set; }
+
+        [SitecoreChildren]
+        public IEnumerable<ArticleItem> ArticleItems { get; set; }
+
+        [SitecoreChildren]
+        public IEnumerable<AwardLogo> AwardLogos { get; set; }
+    }
+
+    public class ArticleItem : SitecoreItem
+    {
+        public ArticleItem()
+        {
+            Image = new Image();
+        }
+
+        public Image Image { get; set; }
+
+        public string LeadIn { get; set; }
+
+        public string Title { get; set; }
+
+        public Link Link { get; set; }
+    }
+
+    public class AwardLogo : SitecoreItem
+    {
+        public AwardLogo()
+        {
+            AwardImage = new Image();
+        }
+        public Image AwardImage { get; set; }
+
+        public Link Link { get; set; }
+
     }
 }
---
+++ Vitality.Website/Extensions/Views/AwardLeaderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glass.Mapper.Sc.Web.Mvc;
+using Vitality.Website.Areas.Presales.ComponentTemplates.FeatureBlocks;
+
+namespace Vitality.Website.Extensions.Views
+{
+    public static class AwardLeaderExtensions
+    {
+        public static int NumberOfColumns(this GlassView<AwardLeader> view)
+        {
+            return 12/view.Model.ArticleItems.Count();
+        }
+
+        public static int NumberOfAwardLogos(this GlassView<AwardLeader> view)
+        {
+            return 12 / view.Model.AwardLogos.Count();
+        }
+
+        public static string BackgroundImage(this GlassView<AwardLeader> view, ArticleItem articleItem)
+        {
+            if (articleItem != null && articleItem.Image != null && !string.IsNullOrWhiteSpace(articleItem.Image.Src))
+            {
+                return string.Format("background-image: url('{0}')", articleItem.Image.Src);
+            }
+            return "";
+        }
+
+        public static string ColumnSplitOne(this GlassView<AwardLeader> view)
+        {
+            if (view.Model.ArticleItems.Count() > 1)
+            {
+                return "grid-col-5-12";
+            }
+            return "grid-col-4-12";
+        }
+
+        public static string ColumnSplitTwo(this GlassView<AwardLeader> view)
+        {
+            if (view.Model.ArticleItems.Count() > 1)
+            {
+                return "grid-col-7-12";
+            }
+            return "grid-col-8-12";
+        }
+    }
+}
+
