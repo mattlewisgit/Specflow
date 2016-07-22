62f7d03b6c0effb85b1e2af77a6b71ee426ef235
Darren Hickling <darren.hickling@vitality.co.uk>
2016-07-22 17:29:18 +0100

    PL-590 DoD Only animating the cookie message the first time *sigh*

--- Boilerplate/src/js/modules/cookieMessageModule.js
+++ Boilerplate/src/js/modules/cookieMessageModule.js
@@ -1,30 +1,48 @@
 ﻿var _container = null;
-var _cookieKey = "hasReadCookieMessage";
+var _cookieReadKey = "hasReadCookieMessage";
+var _cookieSeenKey = "hasSeenCookieMessage";
 var _areCookiesDisabled = false;
+
+var _expiry = {
+    expires: 365
+};
 
 function init() {
     "use strict";
-    // Bomb out if there is no cookie container, or the cookie is set.
+    // Bomb out if there is no cookie container, or the read cookie is set.
     _container = $(".cookie-message");
     _areCookiesDisabled = $("html").hasClass("disable-cookies");
 
-    if (_container.length < 1 || (!_areCookiesDisabled && !!Cookies.get(_cookieKey))) {
+    if (_container.length < 1 || (!_areCookiesDisabled && !!Cookies.get(_cookieReadKey))) {
         return;
     }
 
-    // Show the message and set the cookie.
-    _container.show();
-
-    if (!_areCookiesDisabled) {
-        Cookies.set(_cookieKey, true, { expires: 365 });
+    // Animate the message if it has not been seen before.
+    if (!Cookies.get(_cookieSeenKey)) {
+        _container
+            .addClass("animated")
+            .addClass("slideInDown");
     }
 
-    // Slide out and hide the message on close.
+    // Show the message and stop if from being animated again.
+    _container.show();
+
+    if (!Cookies.get(_cookieSeenKey)) {
+        Cookies.set(_cookieSeenKey, true, _expiry);
+    }
+
+    // Hide the message and stop it from being shown again on accept.
     _container.find(".box-button").click(function() {
         _container
+            .addClass("animated")
             .removeClass("slideInDown")
             .addClass("slideOutUp");
 
+        if (!_areCookiesDisabled) {
+            Cookies.set(_cookieReadKey, true, _expiry);
+        }
+
+        // Hide after animating.
         setTimeout(function () {
             _container.hide();
         }, 500);
