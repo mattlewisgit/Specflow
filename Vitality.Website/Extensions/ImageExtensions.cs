bddbc8b63913374d280b5db066ff0694eee46d11
vivian2 <vivian.roberts@vitality.co.uk>
2016-08-04 17:07:15 +0100

    image resizing

---
+++ Vitality.Website/Extensions/ImageExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Vitality.Website.Extensions
+{
+    using Glass.Mapper;
+    using Glass.Mapper.Sc.Fields;
+
+    public static class ImageExtensions
+    {
+        public static string ProtectedSrc(this Image image, int height = 0, int width = 0)
+        {
+            var src = new UrlBuilder(image.Src);
+            if (height > 0)
+            {
+                src.AddToQueryString("h", height.ToString());
+            }
+            if (width > 0)
+            {
+                src.AddToQueryString("w", width.ToString());
+            }
+            return Sitecore.Resources.Media.HashingUtils.ProtectAssetUrl(src.ToString());
+        }
+    }
+}
