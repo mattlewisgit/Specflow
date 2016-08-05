25d12be3de34223f44d8ee96fb9a3458a5dff010
David Buckell <david.buckell@vitality.co.uk>
2016-08-05 18:02:45 +0100

    refactored sitemaps

---
+++ Vitality.Website.SC/Agents/Sitemaps/SitemapGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Links;
+using Sitecore.Sites;
+
+namespace Vitality.Website.SC.Agents.Sitemaps
+{
+    /// <summary>
+    /// Generates sitemaps and sitemap index file.
+    /// </summary>
+    public class SitemapGenerator
+    {
+        private readonly Database database;
+        private SitemapIndex sitemapIndex;
+
+        public SitemapGenerator(string databaseName)
+        {
+            Error.AssertString(databaseName, "databaseName", false);
+            this.database = Factory.GetDatabase(databaseName);
+        }
+
+        public void Run()
+        {
+            var homeItem = database.GetItem(new ID(ItemConstants.Presales.Content.Home.Id));
+            if (homeItem == null)
+            {
+                throw new ArgumentException("Home item not found.");
+            }
+
+            string baseUrl;
+            using (new SiteContextSwitcher(Factory.GetSite("presales")))
+            {
+                baseUrl = LinkManager.GetItemUrl(homeItem, new UrlOptions { AlwaysIncludeServerUrl = true, LanguageEmbedding = LanguageEmbedding.Never });
+            }
+
+            sitemapIndex = new SitemapIndex(baseUrl, database.GetItem(new ID(ItemConstants.Presales.Content.Configuration.Sitemaps.Id)).Children);
+
+            RecurseContentTree(homeItem);
+
+            sitemapIndex.Build();
+        }
+
+        private void RecurseContentTree(Item item)
+        {
+            sitemapIndex.AddToRelativeSitemap(item);
+
+            if (item.HasChildren)
+            {
+                foreach (Item child in item.Children)
+                {
+                    RecurseContentTree(child);
+                }
+            }
+        }
+    }
+}
---
+++ Vitality.Website.SC/Agents/Sitemaps/SitemapSettings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Sitecore.Data.Items;
+
+namespace Vitality.Website.SC.Agents.Sitemaps
+{
+    public class SitemapSettings
+    {
+        private const string HideFromSitemapField = "HideFromSitemap";
+        private const string InheritSitemapSettingsField = "InheritSitemapSettings";
+        private const string SitemapField = "Sitemap";
+        private const string ChangeFrequencyField = "ChangeFrequency";
+        private const string PriorityField = "Priority";
+
+        public string PageUrl { get; set; }
+        public bool HideFromSitemap { get; set; }
+        public bool InheritSitemapSettings { get; set; }
+        public string SitemapName { get; set; }
+        public string ChangeFrequency { get; set; }
+        public string Priority { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+        public static SitemapSettings From(Item item, string baseUrl)
+        {
+            var relativePath = item.Paths.ContentPath.Remove(0, "/presales/home".Length);
+            if (relativePath.Length > 0)
+            {
+                relativePath = relativePath.Remove(0, 1) + "/";
+            }
+            string itemUrl = baseUrl + relativePath;
+
+            var publishedDate = item.Statistics.Updated;
+            var hideFromSitemap = item[HideFromSitemapField] == "1";
+
+            while (item[InheritSitemapSettingsField] == "1" && item.ID.Guid != ItemConstants.Presales.Content.Home.Id)
+            {
+                item = item.Parent;
+            }
+
+            return new SitemapSettings
+            {
+                PageUrl = itemUrl,
+                ChangeFrequency = item.Database.GetItem(item[ChangeFrequencyField]).Fields["Value"].Value,
+                Priority = item[PriorityField],
+                SitemapName = item.Database.GetItem(item[SitemapField]).Fields["Value"].Value,
+                PublishedDate = publishedDate,
+                HideFromSitemap = hideFromSitemap
+            };
+        }
+    }
+}
