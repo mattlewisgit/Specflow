6aacc7ac44e80ceb780c483f1ec5fef52fdcf891
Janaka Lakmal <janaka.lakmal@vitality.co.uk>
2016-09-06 16:32:47 +0100

    Enable Queryable Datasources for RenderingLayouts.

---
+++ Vitality.Website.SC/Pipelines/Response/GetRenderer/ResolveDatasourceQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Sitecore.Mvc.Pipelines.Response.GetRenderer;
+using Vitality.Website.SC.Utilities;
+
+namespace Vitality.Website.SC.Pipelines.Response.GetRenderer
+{
+    public class ResolveDatasourceQuery : GetRendererProcessor
+    {
+        public override void Process(GetRendererArgs args)
+        {
+            if (args.Rendering.DataSource.StartsWith(QueryHelper.QueryStartText, StringComparison.InvariantCultureIgnoreCase))
+            {
+                args.Rendering.DataSource = args.Rendering.DataSource.ResolveQuery(args.PageContext.Item.Axes);
+            }
+        }
+    }
+}
