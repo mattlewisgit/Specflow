1ad76cafe6a8e93943187eaa381c700d08f91091
Darren Hickling <darren.hickling@vitality.co.uk>
2016-09-13 15:50:44 +0100

    Moving literature library service to separate file
    Is currently a mock and can be swapped for calls to a real API

--- Boilerplate/src/js/angular/healthAdvisersLiteratureApp.js
+++ Boilerplate/src/js/angular/healthAdvisersLiteratureApp.js
@@ -3,85 +3,8 @@
     typeSelected: "typeSelected"
 };
 
-function randomDate(start, end) {
-    end = end || new Date();
-    start = start || new Date(end.getFullYear() - 1, end.getMonth(), end.getDate());
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-}
-
-var mockData = {
-    documents: {
-        "guide-to-cover---business-healthcare-2015": {
-            AvailableLiterature: [],
-            Category: "Business healthcare literature",
-            Code: "PHU1294A",
-            Description: "Business Healthcare with VitalityHealth",
-            Document: "dev.vitality.co.uk/-/media/Presales/pdf/Development/PHF-sales-aid-v2.ashx",
-            PublishDate: randomDate().toJSON(),
-            Thumbnail: "/src/img/examples/example-pdf.png",
-            Title: "Business Healthcare sales aid"
-        },
-        "guide-to-healthy-living-rewards-and-partners-january-2015": {
-            AvailableLiterature: [],
-            Category: "Business healthcare literature",
-            Code: "PHU1294A",
-            Description: "Health and Reward partners with VitalityHealth",
-            Document: "dev.vitality.co.uk/-/media/Presales/pdf/Development/PHF-sales-aid-v2.ashx",
-            PublishDate: randomDate().toJSON(),
-            Thumbnail: "/src/img/examples/example-pdf.png",
-            Title: "Health and Reward partners"
-        },
-        "guide-to-cover---personal-healthcare-2015": {
-            AvailableLiterature: [],
-            Category: "Personal healthcare literature",
-            Code: "PHU1294A",
-            Description: "Personal Healthcare with VitalityHealth",
-            Document: "dev.vitality.co.uk/-/media/Presales/pdf/Development/PHF-sales-aid-v2.ashx",
-            PublishDate: randomDate().toJSON(),
-            Thumbnail: "/src/img/examples/example-pdf.png",
-            Title: "Personal Healthcare sales aid"
-        }
-    },
-    literature: {
-        "Personal healthcare literature": [
-            {
-                CategoryKey: "business-and-corporate-healthcare-literature",
-                Key: "guide-to-cover---business-healthcare-2015",
-                Title: "Guide to Cover - Business Healthcare 2015"
-            },
-            {
-                CategoryKey: "personal-healthcare-literature",
-                Key: "guide-to-cover---personal-healthcare-2015",
-                Title: "Guide to Cover - Personal Healthcare 2015"
-            }
-        ],
-        "Sales literature": [
-            {
-                CategoryKey: "sales-support-and-literature",
-                Key: "guide-to-healthy-living-rewards-and-partners-january-2015",
-                Title: "Health and Reward partners"
-            }
-        ]
-    }
-};
-
 window.healthAdvisersSalesLiteratureApp = angular
-    .module("HealthAdvisersSalesLiteratureApp", [])
-    .factory("dataService", ["$http", function ($http) {
-        var _currentDocument = {};
-
-        return {
-            currentDocument: function() {
-                return _currentDocument;
-            },
-            getDocument: function (key) {
-                return (_currentDocument = mockData.documents[key]);
-            },
-            getLiterature: function (name) {
-                return mockData.literature[name];
-            }
-        };
-    }])
+    .module("HealthAdvisersSalesLiteratureApp", ["LiteratureLibraryService"])
     .controller("ChooseController", ["$scope", "$rootScope", "literatureTypes",
         function ($scope, $rootScope, literatureTypes) {
             "use strict";
@@ -98,12 +21,12 @@ window.healthAdvisersSalesLiteratureApp = angular
                 });
             };
         }])
-    .controller("AvailableController", ["$scope", "$rootScope", "dataService",
-        function ($scope, $rootScope, dataService) {
+    .controller("AvailableController", ["$scope", "$rootScope", "LiteratureLibraryService",
+        function ($scope, $rootScope, LiteratureLibraryService) {
             "use strict";
             $rootScope.$on(events.typeSelected, function (event, literatureType) {
                 // Fetch the data.
-                $scope.literature = dataService.getLiterature(literatureType.Name);
+                $scope.literature = LiteratureLibraryService.getLiterature(literatureType.Name);
 
                 // Deselect all of the documents.
                 $scope.literature.forEach(function (document) {
@@ -121,10 +44,11 @@ window.healthAdvisersSalesLiteratureApp = angular
                 });
             };
         }])
-    .controller("DocumentController", ["$scope", "$rootScope", "dataService",
-        function ($scope, $rootScope, dataService) {
+    .controller("DocumentController", ["$scope", "$rootScope", "LiteratureLibraryService",
+        function ($scope, $rootScope, LiteratureLibraryService) {
             "use strict";
             $rootScope.$on(events.documentedSelected, function (event, document) {
-                $scope.document = dataService.getDocument(document.Key);
+                $scope.document = LiteratureLibraryService.getDocument(document.Key);
             });
         }]);
+ 
