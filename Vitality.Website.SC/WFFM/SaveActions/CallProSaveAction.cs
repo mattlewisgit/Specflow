c770b2a9d58cd31c18dd48779c9d65f26c0b9d5a
David Buckell <david.buckell@vitality.co.uk>
2016-10-07 14:38:06 +0100

    added save action and extract xml from selected xml schema

---
+++ Vitality.Website.SC/WFFM/SaveActions/CallProSaveAction.cs
@@ -0,0 +1,56 @@
+﻿using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.Forms.Core.Wrappers;
+using Sitecore.Security.Authentication;
+using Sitecore.WFFM.Abstractions.Actions;
+
+namespace Vitality.Website.SC.WFFM.SaveActions
+{
+    public class CallProSaveAction : ISaveAction
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallProSaveAction"/> class.
+        /// </summary>
+        public CallProSaveAction()
+        {
+        }
+
+        /// <summary>
+        /// The execute.
+        /// </summary>
+        /// <param name="formId">
+        /// The form id.
+        /// </param>
+        /// <param name="adaptedFields">
+        /// The adapted fields.
+        /// </param>
+        /// <param name="actionCallContext">
+        /// The action call context.
+        /// </param>
+        /// <param name="data">
+        /// The data.
+        /// </param>
+        public void Execute(ID formId, AdaptedResultList adaptedFields, ActionCallContext actionCallContext = null, params object[] data)
+        {
+            string xmlData = data[0].ToString();
+            Assert.ArgumentNotNullOrEmpty(xmlData, "Missing CallPro XML schema.");
+
+            // Call pro integration
+            var item = Sitecore.Context.Database.GetItem(new ID(SchemaId));
+            var xml = item.Fields["Xml"].Value;
+        }
+
+        /// <summary>
+        /// Gets or sets the SchemaId field.
+        /// </summary>
+        public string SchemaId { get; set; }
+
+        public ID ActionID { get; set; }
+        public string UniqueKey { get; set; }
+        public ActionType ActionType { get; private set; }
+        public ActionState QueryState(ActionQueryContext queryContext)
+        {
+            return ActionState.Enabled;
+        }
+    }
+}
---
+++ Vitality.Website.SC/WFFM/SaveActions/CallProSchema.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Sitecore.Web.UI.Pages;
+using Sitecore.Data.Items;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Web.UI.XmlControls;
+using Sitecore.Web.UI.HtmlControls;
+using System.Collections.Specialized;
+using Sitecore.Form.Core.Utility;
+
+namespace Vitality.Website.SC.WFFM.SaveActions
+{
+    public class CallProSchema : DialogForm
+    {
+        protected GenericControl CurrentPlaceholder;
+        protected XmlControl Dialog;
+        protected DataContext ItemDataContext;
+        protected DataTreeview ItemTreeView;
+
+        protected NameValueCollection nvc = new NameValueCollection();
+
+        public string Params
+        {
+            get
+            {
+                return (System.Web.HttpContext.Current.Session[Sitecore.Web.WebUtil.GetQueryString("params")] as string);
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+
+            base.OnLoad(e);
+
+            if (!Sitecore.Context.ClientPage.IsEvent)
+            {
+                //this.Localize();
+
+                nvc = ParametersUtil.XmlToNameValueCollection(Params);
+
+                SchemaId = nvc["SchemaId"];
+
+                if (!string.IsNullOrEmpty(this.SchemaId) && Sitecore.Data.ID.IsID(this.SchemaId))
+                {
+                    this.ItemDataContext.DefaultItem = this.SchemaId;
+                }
+                this.OnNodeSelected(this, EventArgs.Empty);
+            }
+            this.ItemTreeView.OnClick += new EventHandler(this.OnNodeSelected);
+        }
+
+        private void OnNodeSelected(object sender, EventArgs e)
+        {
+            if (this.ItemDataContext.CurrentItem != null)
+            {
+                string str = this.ItemDataContext.CurrentItem.ID.ToString();
+                nvc.Add("SchemaId", str);
+            }
+        }
+
+        protected override void OnOK(object sender, EventArgs args)
+        {
+            Item selectionItem = this.ItemTreeView.GetSelectionItem();
+            if (selectionItem == null)
+            {
+                SheerResponse.Alert("Choose an item", new string[0]);
+            }
+            else
+            {
+                nvc.Add("SchemaId", selectionItem.ID.ToString());
+                string str = ParametersUtil.NameValueCollectionToXml(nvc);
+                if (str.Length == 0)
+                {
+                    str = "-";
+                }
+                SheerResponse.SetDialogValue(str);
+                base.OnOK(sender, args);
+            }
+        }
+
+        public string SchemaId { get; set; }
+    }
+}
