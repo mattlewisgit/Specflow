f793cf1df565e53c842207944e88f4edcd5294c9
Darren Hickling <darren.hickling@vitality.co.uk>
2016-10-13 00:17:59 +0100

    DD-1039 UI Individual Steps
    Adding journey flow
    Using new service to store state
    Also "retrieves" and filters data
    Most of the work is now done, need to hook up Date logic

--- Boilerplate/src/js/angular/healthAdvisersFormApp.js
+++ Boilerplate/src/js/angular/healthAdvisersFormApp.js
@@ -1,22 +1,95 @@
-﻿window.healthAdvisersSalesLiteratureApp = angular
-    .module("HealthAdvisersFormApp", ["LiteratureLibraryService"])
+﻿var events = {
+    dateSubmit: "dateSubmit",
+    planCodeSubmit: "planCodeSubmit",
+    planTypeSubmit: "planTypeSubmit",
+    restart: "restart"
+};
+
+window.healthAdvisersSalesLiteratureApp = angular
+    .module("HealthAdvisersFormApp", ["LiteratureFormService"])
     .controller("PlanTypeStepController", [
         "$scope",
         "$rootScope",
-        function($scope, $rootScope) {
+        "LiteratureFormService",
+        function ($scope, $rootScope, LiteratureFormService) {
             "use strict";
-            $scope.planTypes = [
-                "Business",
-                "Personal",
-                "Corporate"
-            ];
-
+            $scope.isVisible = true;
+            $scope.planTypes = LiteratureFormService.getCategories();
             $scope.selectedPlanType = $scope.planTypes[1];
 
-            $scope.typeSelected = function (data) {
-                debugger;
-                //$scope.selectedPlanType
-                //$rootScope.$broadcast(events.dateFilter, actualDate);
+            $rootScope.$on(events.restart, function (event) {
+                $scope.isVisible = true;
+            });
+
+            $scope.typeSelected = function () {
+                this.isVisible = false;
+                LiteratureFormService.setPlanType(this.selectedPlanType);
+                $rootScope.$broadcast(events.planTypeSubmit);
+            };
+        }
+    ])
+    .controller("DateStepController", [
+        "$scope",
+        "$rootScope",
+        function ($scope, $rootScope) {
+            "use strict";
+            $scope.isVisible = false;
+
+            $rootScope.$on(events.planTypeSubmit, function (event) {
+                $scope.isVisible = true;
+            });
+
+            $scope.dateEntered = function () {
+                this.isVisible = false;
+                $rootScope.$broadcast(events.dateSubmit);
+            };
+        }
+    ])
+    .controller("PlanCodeController", [
+        "$scope",
+        "$rootScope",
+        "LiteratureFormService",
+        function ($scope, $rootScope, LiteratureFormService) {
+            "use strict";
+            $scope.isVisible = false;
+            $scope.planCodes = [];
+            $scope.selectedPlanCode = "";
+
+            $rootScope.$on(events.dateSubmit, function (event) {
+                $scope.planCodes = LiteratureFormService.getPlanCodes();
+                $scope.selectedPlanCode = $scope.planCodes[0];
+                $scope.isVisible = true;
+            });
+
+            $scope.planCodeSubmit = function () {
+                this.isVisible = false;
+                LiteratureFormService.setPlanCode(this.selectedPlanCode);
+                $rootScope.$broadcast(events.planCodeSubmit);
+            };
+        }
+    ])
+    .controller("ResultsController", [
+        "$scope",
+        "$rootScope",
+        "LiteratureFormService",
+        function ($scope, $rootScope, LiteratureFormService) {
+            "use strict";
+            $scope.badges = [];
+            $scope.documents = [];
+
+            $scope.isVisible = false;
+
+            $rootScope.$on(events.planCodeSubmit, function (event) {
+                var formState = angular.copy(LiteratureFormService.getFormState());
+                formState.date = formState.date.toDateString();
+                $scope.badges = _.values(formState);
+                $scope.documents = LiteratureFormService.getDocuments();
+                $scope.isVisible = true;
+            });
+
+            $scope.restart = function () {
+                this.isVisible = false;
+                $rootScope.$broadcast(events.restart);
             };
         }
     ]);
---
+++ Boilerplate/src/js/mocks/literatureFormServiceMock.js
@@ -0,0 +1,120 @@
+﻿// jscs:disable maximumLineLength
+var mockData = {
+    documents: [
+        {
+            AvailableLiterature: [],
+            Category: "Business",
+            Code: "PHU1234",
+            Description: "Vitality GP Select with VitalityHealth",
+            Document: "http://dev.vitality.co.uk/media-online/presales/pdf/development/guide-to-business-healthcare---january-2015-print-friendly.pdf",
+            EffectivePlanDate: new Date("2015-07-06T23:00:00Z"),
+            Key: "vitality-gp-select",
+            PublishDate: "2016-06-01T23:00:00Z",
+            Size: 330,
+            Thumbnail: "/src/img/examples/example-pdf.png",
+            Title: "Personal Healthcare - New Business Only"
+        },
+        {
+            AvailableLiterature: [],
+            Category: "Personal",
+            Code: "PLU1294A",
+            Description: "Vitality GP with VitalityHealth",
+            Document: "http://dev.vitality.co.uk/media-online/presales/pdf/development/phf-sales-aid-v2.pdf",
+            EffectivePlanDate: new Date("2016-01-06T23:00:00Z"),
+            Key: "vitality-gp",
+            PublishDate: "2016-07-06T23:00:00Z",
+            Size: 200,
+            Thumbnail: "/src/img/examples/example-pdf.png",
+            Title: "Personal Healthcare - Business Only"
+        },
+        {
+            AvailableLiterature: [],
+            Category: "Corporate",
+            Code: "NPU1234",
+            Description: "Health and rewards partners with VitalityHealth",
+            Document: "http://dev.vitality.co.uk/media-online/presales/pdf/development/business-healthcare-sales-aid---january-2015.pdf",
+            EffectivePlanDate: new Date("2016-04-20T23:00:00Z"),
+            Key: "health-and-rewards-partners",
+            PublishDate: "2016-06-01T23:00:00Z",
+            Size: 220,
+            Thumbnail: "/src/img/examples/example-pdf.png",
+            Title: "Personal Healthcare - Moratorium - Business Only"
+        },
+        {
+            AvailableLiterature: [],
+            Category: "Corporate",
+            Code: "PHU1294A",
+            Description: "Personal Healthcare with VitalityHealth",
+            Document: "http://dev.vitality.co.uk/media-online/presales/pdf/development/phf-sales-aid-v2.pdf",
+            EffectivePlanDate: new Date("2016-03-06T23:00:00Z"),
+            Key: "personal-healthcare-sales-aid",
+            PublishDate: "2016-07-06T23:00:00Z",
+            Size: 1024,
+            Thumbnail: "/src/img/examples/example-pdf.png",
+            Title: "Personal Healthcare - New Business"
+        }
+    ]
+};
+
+angular
+    .module("LiteratureFormService", [])
+    .service("LiteratureFormService", function () {
+        "use strict";
+        this._formState = {
+            date: new Date(),
+            planCode: "",
+            planType: "",
+        };
+
+        this._planCodes = [];
+
+        var documentProperties = {
+            Title: "Title"
+        };
+
+        this.getCategories = function () {
+            return _.uniq(_.pluck(mockData.documents, "Category"));
+        };
+
+        this.getDocuments = function () {
+            return _.sortBy(mockData.documents
+                .filter(function (document) {
+                    return document.Category == this._formState.planType &&
+                        document.Code.indexOf(this._formState.planCode) === 0;
+                }, this)
+                .map(function (document) {
+                    document.FileType = _.last(document.Document.split(".")).toUpperCase();
+                    return document;
+                }), documentProperties.Title);
+        };
+
+        this.getFormState = function () {
+            return this._formState;
+        };
+
+        this.getPlanCodes = function (category) {
+            return _.uniq(_.pluck(mockData.documents, "Code").map(function (planCode) {
+                return planCode.substr(0, 2);
+            })).sort();
+        };
+
+        this.setDate = function (date) {
+            this._formState.date = date;
+        }
+
+        this.setPlanCode = function (planCode) {
+            this._formState.planCode = planCode;
+        }
+
+        this.setPlanType = function (planType) {
+            this._formState.planType = planType;
+        }
+
+        this.reset = function () {
+            this._formState = {
+                date: new Date(),
+                planCode: "",
+                planType: "",
+            };
+        };
+    });
