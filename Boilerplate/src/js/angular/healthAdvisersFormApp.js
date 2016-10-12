c8dbcf6a291324324b7d8108ff8b51a562321b46
Darren Hickling <darren.hickling@vitality.co.uk>
2016-10-12 21:33:49 +0100

    DD-1039 UI Individual Steps
    Adding initial Angular app and switching to the laptop

---
+++ Boilerplate/src/js/angular/healthAdvisersFormApp.js
@@ -0,0 +1,22 @@
+﻿window.healthAdvisersSalesLiteratureApp = angular
+    .module("HealthAdvisersFormApp", ["LiteratureLibraryService"])
+    .controller("PlanTypeStepController", [
+        "$scope",
+        "$rootScope",
+        function($scope, $rootScope) {
+            "use strict";
+            $scope.planTypes = [
+                "Business",
+                "Personal",
+                "Corporate"
+            ];
+
+            $scope.selectedPlanType = $scope.planTypes[1];
+
+            $scope.typeSelected = function (data) {
+                debugger;
+                //$scope.selectedPlanType
+                //$rootScope.$broadcast(events.dateFilter, actualDate);
+            };
+        }
+    ]);
