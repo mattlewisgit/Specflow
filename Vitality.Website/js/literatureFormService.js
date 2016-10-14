a2fbec011ccd246c8124d1b1df487a4c76dc0e7f
Darren Hickling <darren.hickling@vitality.co.uk>
2016-10-14 12:49:51 +0100

    DD-1038 Render media library items as data to the page
    Cleaning up access to form state
    Adding new label link styling and links backs to form steps

--- Vitality.Website/js/literatureFormService.js
+++ Vitality.Website/js/literatureFormService.js
@@ -6,18 +6,30 @@
         var cachedPlanTypes = [];
         var cachedDocuments = [];
 
-        this._formState = {
+        var documentProperties = {
+            PlanNumber: "PlanNumber",
+            PlanType: "PlanType",
+            Title: "Title"
+        };
+
+        this.formState = {
             date: moment(),
             planCode: "",
             planType: "",
         };
 
-        this._planCodes = [];
-
-        var documentProperties = {
-            PlanNumber: "PlanNumber",
-            PlanType: "PlanType",
-            Title: "Title"
+        this.getDocuments = function () {
+            return _.sortBy(cachedDocuments
+                .filter(function (document) {
+                    // Filter documents based on the plan number, effective date and plan type.
+                    return (+document.PlanNumber) === (+this.formState.planCode) &&
+                        moment(document.EffectivePlanDate).isAfter(this.formState.date) &&
+                        document.PlanType === this.formState.planType;
+                }, this)
+                .map(function (document) {
+                    document.FileType = _.last(document.Document.split(".")).toUpperCase();
+                    return document;
+                }), documentProperties.Title);
         };
 
         this.getPlanCodes = function () {
@@ -40,34 +52,4 @@
                     callback(cachedPlanTypes);
                 });
         };
-
-        this.getDocuments = function () {
-            return _.sortBy(cachedDocuments
-                .filter(function (document) {
-                    // Filter documents based on the plan number, effective date and plan type.
-                    return (+document.PlanNumber) === (+this._formState.planCode) &&
-                        moment(document.EffectivePlanDate).isAfter(this._formState.date) &&
-                        document.PlanType === this._formState.planType;
-                }, this)
-                .map(function (document) {
-                    document.FileType = _.last(document.Document.split(".")).toUpperCase();
-                    return document;
-                }), documentProperties.Title);
-        };
-
-        this.getFormState = function () {
-            return this._formState;
-        };
-
-        this.setDate = function (date) {
-            this._formState.date = date;
-        }
-
-        this.setPlanCode = function (planCode) {
-            this._formState.planCode = planCode;
-        }
-
-        this.setPlanType = function (planType) {
-            this._formState.planType = planType;
-        }
     }]);
