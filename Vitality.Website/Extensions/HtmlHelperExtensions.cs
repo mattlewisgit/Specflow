67b9a51f45e8889e2e6fa9c34429d17387106b8b
Darren Hickling <darren.hickling@vitality.co.uk>
2016-10-20 14:46:25 +0100

    DD-1120 Add form styling
    Changing the markup for the main WFM fields
    Adding HTML helpers for checkbox and radio lists
    Now to change the error highlighting and this is mostly there

---
+++ Vitality.Website/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace Vitality.Website.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Web.Mvc;
+
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString CheckBoxList
+            (this HtmlHelper helper, string name, IEnumerable<SelectListItem> items)
+        {
+            return InputList("checkbox", name, items);
+        }
+
+        public static MvcHtmlString RadioButtonList
+            (this HtmlHelper helper, string name, IEnumerable<SelectListItem> items)
+        {
+            return InputList("radio", name, items);
+        }
+
+        private static MvcHtmlString InputList
+            (string inputType, string name, IEnumerable<SelectListItem> items)
+        {
+            var output = new StringBuilder("<fieldset>");
+
+            foreach (var item in items)
+            {
+                output.Append("<div class=\"form__field__input form__field__input--checkbox\">");
+
+                var inputLabel = string.Format(
+                    "<input type=\"{0}\" name=\"{1}\" value=\"{2}\" {3} id=\"{4}\" /><label for=\"{4}\">{5}</label>",
+                    inputType, name, item.Value, (item.Selected) ? "checked" : string.Empty, Guid.NewGuid(), item.Text);
+
+                output.Append(inputLabel).Append("</div>");
+            }
+
+            output.Append("</fieldset>");
+            return new MvcHtmlString(output.ToString());
+        }
+    }
+}
