34168976475ec8db1d0913cd783d22216e9b499c
Darren Hickling <darren.hickling@vitality.co.uk>
2016-10-27 17:17:01 +0100

    DD-1226 Test WFFM CallPro integration
    Cleaning up code and adding quick wins

---
+++ Vitality.Website.SC/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Vitality.Website.SC.Extensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class StringExtensions
+    {
+        public static string Replace(this string source, IDictionary<string, string> replacements)
+        {
+            return replacements.Aggregate(source, (agg, kvp) => agg.Replace(kvp.Key, kvp.Value));
+        }
+    }
+}
