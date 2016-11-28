8ca4b139ec6912889b11c09728cd123ccf005a2d
Darren Hickling <darren.hickling@vitality.co.uk>
2016-11-28 17:17:50 +0000

    DD-1489 Fix it Using new, unit-tested extension to overwrite a dictionary key's value  if the key already exists, rather than throw an exception

---
+++ Vitality.Website/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Vitality.Website.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class IDictionaryExtensions
+    {
+        public static IDictionary<TKey, TValue> AddOrOverwrite<TKey, TValue>
+            (this IDictionary<TKey, TValue> source, TKey key, TValue value)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (source.ContainsKey(key))
+            {
+                source.Remove(key);
+            }
+
+            source.Add(key, value);
+            return source;
+        }
+    }
+}
