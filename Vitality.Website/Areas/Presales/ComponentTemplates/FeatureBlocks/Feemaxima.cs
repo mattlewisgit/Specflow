384bb7d46f2309fec3f88583945dd25641fa9014
Janaka Lakmal <JANAKA1@vitality.com>
2016-12-06 11:31:42 +0000

    Read from Sitecore Template

--- Vitality.Website/Areas/Presales/ComponentTemplates/FeatureBlocks/Feemaxima.cs
+++ Vitality.Website/Areas/Presales/ComponentTemplates/FeatureBlocks/Feemaxima.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using Glass.Mapper.Sc.Configuration.Attributes;
 using Glass.Mapper.Sc.Fields;
+using Sitecore.Globalization;
 using Vitality.Website.Areas.Global.Models;
 using Vitality.Website.Areas.Presales.ComponentTemplates.Generic;
 
@@ -12,12 +13,20 @@ namespace Vitality.Website.Areas.Presales.ComponentTemplates.FeatureBlocks
     [SitecoreType(AutoMap = true)]
     public class FeeMaxima : SitecoreItem
     {
-        public string Headline { get; set; }
-
+        public FeeMaxima()
+        {
+            CurrencySymbol = "£";
+        }
+        public string AnaesthetistsFeeHeader { get; set; }
         public string BackLinkText { get; set; }
-
+        public string ChapterText { get; set; }
+        public string CodeHeader { get; set; }
+        public string CurrencySymbol { get; set; }
+        public string DescriptionHeader { get; set; }
+        public string Headline { get; set; }
+        public string NoResultsText { get; set; }
         public string SearchText { get; set; }
-
         public string SearchPlaceholderText { get; set; }
+        public string SurgeonsFeeHeader { get; set; }
     }
 }
