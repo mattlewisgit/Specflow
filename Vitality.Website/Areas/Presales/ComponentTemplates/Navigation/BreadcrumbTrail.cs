989ee149d5c95a8bbaa41d89de6303a986e0d466
Janaka Lakmal <JANAKA1@vitality.com>
2016-12-09 11:02:05 +0000

    add breadcrumb class

---
+++ Vitality.Website/Areas/Presales/ComponentTemplates/Navigation/BreadcrumbTrail.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Data.Items;
+using Sitecore.Sites;
+using Vitality.Website.Areas.Presales.Models;
+using Vitality.Website.SC.Utilities;
+
+namespace Vitality.Website.Areas.Presales.ComponentTemplates.Navigation
+{
+    public class BreadcrumbTrail
+    {
+        public ICollection<Breadcrumb> Breadcrumbs { get; set; }
+
+        public BreadcrumbTrail()
+        {
+            var currentItem = Sitecore.Context.Item;
+            var site = SiteContext.Current;
+            var homeItem = site.StartItem;
+
+            Breadcrumbs = new List<Breadcrumb>();
+
+            var index = 0;
+            while (currentItem != null)
+            {
+                Breadcrumbs.Add(new Breadcrumb
+                {
+                  Name =currentItem.Name,
+                  Url = currentItem.Uri.Path,
+                  Position  = index
+                });
+
+                if (currentItem.Paths.Path  == homeItem)
+                    break;
+
+                currentItem = currentItem.Parent;
+            }
+
+            Breadcrumbs.Reverse();
+        }
+    }
+}
