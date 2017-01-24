393a7fe32404b3fdf653fb5f06d29a180b9010e6
Matt Lewis <matt.lewis@vitality.co.uk>
2017-01-24 17:34:22 +0000

    Checking in Specflow Tests for Accordion Component

---
+++ IntegrationTests/Vitality.Website.IntegrationTests/Features/Accordion.feature
@@ -0,0 +1,26 @@
+﻿Feature: Accordion Component
+	In order to avoid silly mistakes
+	As a accordion user
+	I want to be expand and retract the accordion
+
+
+@SIT
+Scenario: Check Accordion Expands on Page Load
+	Given I am on presales /dev/accordion-content
+	Then I expect the accordion description '#tab1' to appear
+
+@SIT
+Scenario: Check Accordion Expands on Click
+	Given I am on presales /dev/accordion-content
+	When I click on accordion tab 'This is Tab 2'
+	Then I expect the accordion description '#tab2' to appear
+
+@SIT
+Scenario: Check Accordion Expands Multiple Times
+	Given I am on presales /dev/accordion-content
+	When I click on accordion tab 'This is Tab 2'
+	Then I expect the accordion description '#tab2' to appear
+	When I click on accordion tab 'This is Tab 3'
+	Then I expect the accordion description '#tab3' to appear
+	When I click on accordion tab 'This is Tab 4'
+	Then I expect the accordion description '#tab4' to appear
---
+++ IntegrationTests/Vitality.Website.IntegrationTests/Steps/AccordionSteps.cs
@@ -0,0 +1,48 @@
+﻿namespace Vitality.Website.IntegrationTests.Steps
+{
+    using System.Drawing;
+    using Selenium.WebDriver.Extensions.JQuery;
+    using Shouldly;
+    using TechTalk.SpecFlow;
+    using Extensions;
+    using Utilities;
+    using OpenQA.Selenium.Interactions;
+    using By = OpenQA.Selenium.By;
+    using OpenQA.Selenium.Support.UI;
+    using System;
+
+
+    [Binding]
+    public sealed class AccordionSteps : BaseSteps
+    {
+
+        [When(@"I click on accordion tab '(.*)'")]
+        public void WhenIClickOnAccordionTab(string p0)
+        {
+            WebDriver
+                    .FindElement(By.LinkText(p0))
+                    .Click();
+        }
+
+
+        [Then(@"I expect the accordion description '(.*)' to appear")]
+        public void ThenIExpectTheAccordionDescriptionToAppear(string p0)
+        {
+            //Check selected accordion description is visible
+            WebDriver
+                    .WaitForElement(new JQuerySelector(p0 + ".expander__content.is-active"))
+                    .Displayed.ShouldBeTrue();
+
+            //Check selected accordion down arrow icon is visible
+            WebDriver
+                    .WaitForElement(new JQuerySelector(".expander__link.is-active"))
+                    .Displayed.ShouldBeTrue();
+            
+        }
+
+        
+
+    }
+
+    
+}
