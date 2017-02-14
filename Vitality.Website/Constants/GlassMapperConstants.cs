1a0d3b8d192475363b2afee3a338c034af6c4e38
David Buckell <david.buckell@vitality.co.uk>
2017-02-14 16:27:17 +0000

    remove redundant code

--- Vitality.Website/Constants/GlassMapperConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Vitality.Website.Constants
-{
-    public static  class GlassMapperConstants
-    {
-        public static string ImageTagFormat = "<img src={2}{0}{2} {1}/>";
-        public static string LazyImageTagFormat = "<img data-src={2}{0}{2} {1}/>";
-    }
-}
