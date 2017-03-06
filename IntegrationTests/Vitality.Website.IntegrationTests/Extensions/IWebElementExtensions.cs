d7418447edc823f66309834871eead9138eafc17
Darren Hickling <darren.hickling@vitality.co.uk>
2017-03-06 17:48:43 +0000

    Was looking at something unrelated, then saw and made some small improvements to Lit Library testing

---
+++ IntegrationTests/Vitality.Website.IntegrationTests/Extensions/IWebElementExtensions.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Vitality.Website.IntegrationTests.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="IWebElement">web elements</see>.
+    /// </summary>
+    public static class IWebElementExtensions
+    {
+        /// <summary>
+        /// Clears the elements and returns it fluently.
+        /// </summary>
+        /// <param name="source">Web element</param>
+        /// <returns>Web element</returns>
+        /// <exception cref="ArgumentNullException">When the element is null</exception>
+        public static IWebElement ClearAndContinue(this IWebElement source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            source.Clear();
+            return source;
+        }
+
+        /// <summary>
+        /// Clicks the elements and returns it fluently.
+        /// </summary>
+        /// <param name="source">Web element</param>
+        /// <returns>Web element</returns>
+        /// <exception cref="ArgumentNullException">When the element is null</exception>
+        public static IWebElement ClickAndContinue(this IWebElement source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            source.Click();
+            return source;
+        }
+    }
+}
--- IntegrationTests/Vitality.Website.IntegrationTests/Steps/LiteratureLibrarySteps.cs
+++ IntegrationTests/Vitality.Website.IntegrationTests/Steps/LiteratureLibrarySteps.cs
@@ -1,58 +1,38 @@
-﻿using TechTalk.SpecFlow;
-
-namespace Vitality.Website.IntegrationTests.Steps
+﻿namespace Vitality.Website.IntegrationTests.Steps
 {
-
-    using System.Drawing;
-
-    using Selenium.WebDriver.Extensions.JQuery;
-
     using Shouldly;
-
     using TechTalk.SpecFlow;
-
     using Extensions;
-    using Utilities;
-
-    using OpenQA.Selenium.Interactions;
     using By = OpenQA.Selenium.By;
-
 
     [Binding]
     public sealed class LiteratureLibrarySteps : BaseSteps
     {
-        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
-
-
-
         [When(@"I search for (.*) document")]
-        public void WhenISearchForDocument(string p0)
+        public void ISearchForDocument(string p0)
         {
-            // Click in the search box
+            // Click in the search box and type the document name.
             WebDriver.
                 FindElement(By.CssSelector("input.tt-input"))
-                .Click();
-            // type in document name
-            WebDriver.
-                FindElement(By.CssSelector("input.tt-input"))
+                .ClickAndContinue()
                 .SendKeys(p0);
-            // click on document name in drop down list
+            
+            // Click on the document name in the drop down list.
             WebDriver.
                 FindElement(By.CssSelector("strong.tt-highlight")).
                 Click();
         }
 
-
-        [Then(@"I expect the download and email  buttons to be visible")]
-        public void ThenIExpectTheDownloadAndEmailButtonsToBeVisible()
+        [Then(@"I expect the download and email buttons to be visible")]
+        public void IExpectTheDownloadAndEmailButtonsToBeVisible()
         {
-            // Download button is visible
+            // Download button is visible.
             WebDriver
                 .WaitForElement(By.LinkText("Download"))
                 .Displayed
                 .ShouldBeTrue();
 
-            // Email button is visible
+            // Email button is visible.
             WebDriver
                 .WaitForElement(By.LinkText("Email"))
                 .Displayed
@@ -60,7 +40,7 @@ namespace Vitality.Website.IntegrationTests.Steps
         }
 
         [Then(@"I expect the (.*) document to be visible")]
-        public void ThenIExpectTheDocumentToBeVisible(string p0)
+        public void IExpectTheDocumentToBeVisible(string p0)
         {
             WebDriver
                 .WaitForElement(By.LinkText(p0))
@@ -68,70 +48,52 @@ namespace Vitality.Website.IntegrationTests.Steps
                 .ShouldBeTrue();
         }
 
-
-
         [When(@"I choose Literature Type (.*)")]
-        public void WhenIChooseLiteratureType(string p0)
+        public void IChooseLiteratureType(string p0)
         {
             WebDriver
-                    .WaitForElement(By.LinkText(p0))
-                    .Displayed
-                    .ShouldBeTrue();
+                .WaitForElement(By.LinkText(p0))
+                .Displayed
+                .ShouldBeTrue();
 
             WebDriver
                 .FindElement(By.LinkText(p0))
                 .Click();
         }
-
 
         [When(@"I select on (.*) Literature")]
-        public void WhenISelectOnLiterature(string p0)
+        public void ISelectOnLiterature(string p0)
         {
             WebDriver
                 .FindElement(By.LinkText(p0))
                 .Click();
         }
 
-
         [Given(@"I enter plan start date (.*) (.*) (.*)")]
-        public void GivenIEnterPlanStartDate(string p0, string p1, string p2)
+        public void IEnterPlanStartDate(string p0, string p1, string p2)
         {
             WebDriver
                 .FindElement(By.Id("day"))
-                .Clear();
-
-            WebDriver
-                .FindElement(By.Id("day"))
+                .ClearAndContinue()
                 .SendKeys(p0);
 
             WebDriver
                 .FindElement(By.Id("month"))
-                .Clear();
-
-            WebDriver
-                .FindElement(By.Id("month"))
+                .ClearAndContinue()
                 .SendKeys(p1);
 
             WebDriver
                 .FindElement(By.Id("year"))
-                .Clear();
-
-            WebDriver
-                .FindElement(By.Id("year"))
+                .ClearAndContinue()
                 .SendKeys(p2);
         }
 
-
         [When(@"click on the submit button")]
-        public void WhenClickOnTheSubmitButton()
+        public void ClickOnTheSubmitButton()
         {
             WebDriver
                 .FindElement(By.XPath("//input[@value='Submit']"))
                 .Click();
         }
-
-
-
-
     }
 }
