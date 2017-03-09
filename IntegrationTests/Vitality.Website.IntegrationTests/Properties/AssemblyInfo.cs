d44826ab364669532b1753352168ef744f711939
David Buckell <david.buckell@vitality.co.uk>
2017-03-09 16:30:25 +0000

    tried saving file in utf8 format

--- IntegrationTests/Vitality.Website.IntegrationTests/Properties/AssemblyInfo.cs
+++ IntegrationTests/Vitality.Website.IntegrationTests/Properties/AssemblyInfo.cs
@@ -16,6 +16,7 @@ using System.Reflection;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5e88d69f-08b6-4c72-a3d8-e107b853a885")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
