3cc1fcc1e11f84fad50b483075a58c06df4145ac
David Buckell <david.buckell@vitality.co.uk>
2017-03-09 16:15:37 +0000

    attempt to fix jenkins (adding ? to start of file)

--- IntegrationTests/Vitality.Website.IntegrationTests/Properties/AssemblyInfo.cs
+++ IntegrationTests/Vitality.Website.IntegrationTests/Properties/AssemblyInfo.cs
@@ -16,7 +16,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5e88d69f-08b6-4c72-a3d8-e107b853a885")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
