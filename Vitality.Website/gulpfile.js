c46bfd343fdbfcd8c4f9d1e94a355c05e34fe27e
Darren Hickling <darren.hickling@vitality.co.uk>
2017-03-23 21:46:40 +0000

    DD-2011 Minify Assets and defer loading Moving the client side assets to the root of the website directory Improving the Gulp build to run Bower installs Deferring all JavaScript and using a single, minified Presales JS include Preparing to use new Boilerplate assets

---
+++ Vitality.Website/gulpfile.js
@@ -0,0 +1,74 @@
+// jshint strict: false
+
+var del = require("del");
+var gulp = require("gulp");
+var runSequence = require("run-sequence");
+
+// Pull all Gulp plugins into a single object.
+var plugins = require("gulp-load-plugins")({
+    pattern: ["gulp[\-\.]*"],
+    replaceString: /\bgulp[\-\.]/
+});
+
+// Set paths that are used eveywhere.
+var paths = {
+    config: "./config/",
+    css: "./css",
+    js: {
+        dest: "./js/",
+        minified: "vitality.presales.min.js",
+        src: "./src/js/*.js"
+    },
+    sass: "./src/sass/**/*.scss"
+}
+
+// Include all configuration files in a single object.
+var configs = {
+    cssnano: require(paths.config + "cssnano-config")
+};
+
+// Task listing.
+gulp.task("help", plugins.taskListing);
+
+// CSS SQA.
+gulp.task("css:lint", function () {
+    return gulp
+        .src(paths.sass)
+        .pipe(plugins.sassLint())
+        .pipe(plugins.sassLint.format())
+        .pipe(plugins.sassLint.failOnError());
+});
+
+// Compiles all SASS to CSS.
+gulp.task("css", ["css:lint"], function () {
+    return gulp
+        .src(paths.sass)
+        .pipe(plugins.sass().on("error", plugins.sass.logError))
+        .pipe(plugins.cssnano(configs.cssnano))
+        .pipe(gulp.dest(paths.css));
+});
+
+// Concatenates and minifies all custom Presales scripts.
+gulp.task("js", function () {
+    return gulp
+        .src(paths.js.src)
+        .pipe(plugins.concat(paths.js.minified))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(paths.js.dest));
+});
+
+// Standard task runner that installs and builds eveything.
+gulp.task("default", function () {
+    runSequence(
+        "bower",
+        [
+            "css",
+            "js"
+        ]
+    );
+});
+
+// Simply runs bower, but saves running the command separately.
+gulp.task("bower", function () {
+    return plugins.bower();
+});
