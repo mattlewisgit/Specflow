b6f5250573990d9d9ffecab22878734317e56b0a
Matt Lewis <matt.lewis@vitality.co.uk>
2017-03-30 17:55:11 +0100

    Added Cards Stacked Tests for SIT - mobile responsive and full-size view

---
+++ IntegrationTests/Vitality.Website.IntegrationTests/Features/CardsStacked.feature
@@ -0,0 +1,29 @@
+﻿Feature: CardsStacked
+	In order to avoid silly mistakes
+	As a CardsStacked user
+	I want to be able to check cards stacked works
+
+
+	@SIT
+Scenario: Check Cards Stacked full screen view
+	Given I am on presales /dev/cards-stacked
+	When I resize to full-screen view
+	Then I expect the correct CSS values to appear for full-screen view
+	
+	@SIT
+Scenario: Check Cards Stacked mobile view
+	Given I am on presales /dev/cards-stacked
+	When I resize to mobile view
+	Then I expect the correct CSS values to appear for mobile view
+
+	@SIT
+Scenario: Check Cards Stacked internal link
+	Given I am on presales /dev/cards-stacked
+	When I go to the Internal Link cards stacked and I click on the Click Internal Link page link
+	Then I see the /dev page
+
+	@SIT
+Scenario: Check Cards Stacked external link
+	Given I am on presales /dev/cards-stacked
+	When I go to the External Link cards stacked and I click on the Click External Link page link
+	Then I see the www.google.co.uk page
---
+++ IntegrationTests/Vitality.Website.IntegrationTests/Steps/CardsStackedSteps.cs
@@ -0,0 +1,63 @@
+﻿namespace Vitality.Website.IntegrationTests.Steps
+{
+    using System.Drawing;
+    using Selenium.WebDriver.Extensions.JQuery;
+    using Shouldly;
+    using TechTalk.SpecFlow;
+    using Extensions;
+    using Utilities;
+    using OpenQA.Selenium.Interactions;
+    using By = OpenQA.Selenium.By;
+    using OpenQA.Selenium.Support.UI;
+    using System;
+
+
+    [Binding]
+    public sealed class CardsStackedSteps : BaseSteps
+    {
+
+
+
+
+        [Given(@"I expect the correct CSS values to appear for full-screen view")]
+        [When(@"I expect the correct CSS values to appear for full-screen view")]
+        [Then(@"I expect the correct CSS values to appear for full-screen view")]
+        public void GivenIExpectTheCorrectCSSValuesToAppearForFull_ScreenView()
+        {
+            String padding = WebDriver.FindElement(new JQuerySelector(".cards-stacked.background-position-top-right.lazyloaded"))
+                       .GetCssValue("padding");
+
+            String backgroundsize = WebDriver.FindElement(new JQuerySelector(".cards-stacked.background-position-top-right.lazyloaded"))
+                      .GetCssValue("background-size");
+
+            padding.ShouldBe("20px");
+            backgroundsize.ShouldBe("cover");
+        }
+
+
+
+        [Given(@"I expect the correct CSS values to appear for mobile view")]
+        [When(@"I expect the correct CSS values to appear for mobile view")]
+        [Then(@"I expect the correct CSS values to appear for mobile view")]
+        public void GivenIExpectTheCorrectCSSValuesToAppearForMobileView()
+        {
+            String padding = WebDriver.FindElement(new JQuerySelector(".cards-stacked.background-position-top-right.lazyloaded"))
+                      .GetCssValue("padding");
+
+            String backgroundsize = WebDriver.FindElement(new JQuerySelector(".cards-stacked.background-position-top-right.lazyloaded"))
+                      .GetCssValue("background-size");
+
+            String backgroundcolor = WebDriver.FindElement(new JQuerySelector(".cards-stacked.background-position-top-right.lazyloaded"))
+                      .GetCssValue("background-color");            
+
+            padding.ShouldBe("40px");
+            backgroundsize.ShouldBe("auto");
+            //rgba(234, 239, 244, 1) = mobile view default colour
+            backgroundcolor.ShouldBe("rgba(234, 239, 244, 1)");
+
+
+        }
+
+
+    }
+}
