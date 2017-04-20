f1a0154c75165fbd5e4faa73a89f85db0d2c5602
David Buckell <david.buckell@vitality.co.uk>
2017-04-20 17:02:11 +0100

    gitignore /js

--- Vitality.Website/js/vacancyDetails-Main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var vacancyDetails_app_module_1 = require('./app/vacancyDetails-app.module');
-var core_1 = require('@angular/core');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(vacancyDetails_app_module_1.VacancyDetailsAppModule);
-// Enable production mode unless running locally
-if (!/presales.vitality.co.uk/.test(document.location.host)) {
-    core_1.enableProdMode();
-}
