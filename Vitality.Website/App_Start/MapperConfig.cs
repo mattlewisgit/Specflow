d5deb2edc5afe28d172c491540b4ccdb23ede32c
Darren Hickling <darren.hickling@vitality.co.uk>
2017-04-27 16:13:20 +0100

    DD-2211 Test

---
+++ Vitality.Website/App_Start/MapperConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace Vitality.Website.App_Start
+{
+    using Areas.Presales.ComponentTemplates.FeatureBlocks;
+    using AutoMapper;
+    using System.Linq;
+
+    public static class MapperConfig
+    {
+        public static void Init()
+        {
+            Mapper.Initialize(SetMapping);
+            Mapper.Configuration.AssertConfigurationIsValid();
+        }
+
+        private static void SetMapping(IMapperConfigurationExpression config)
+        {
+            config
+                .CreateMap<VacancyList, VacancyListViewModel>()
+                .ForMember(
+                    dest => dest.Departments,
+                    opt => opt.MapFrom(src => src.Departments.Select(l => l.Name)))
+                .ForMember(
+                    dest => dest.FeedSettingsEndpoint,
+                    opt => opt.MapFrom(src => src.FeedSettings.Url))
+                .ForMember(
+                    dest => dest.FeedSettingsMockDataFileUrl,
+                    opt => opt.MapFrom(src => src.FeedSettings.MockDataFile))
+                .ForMember(
+                    dest => dest.Locations,
+                    opt => opt.MapFrom(src => src.Locations.Select(l => l.Name)));
+        }
+    }
+}
--- Vitality.Website/Areas/Presales/ComponentTemplates/FeatureBlocks/VacancyListViewModel.cs
+++ Vitality.Website/Areas/Presales/ComponentTemplates/FeatureBlocks/VacancyListViewModel.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Vitality.Website.Areas.Presales.ComponentTemplates.FeatureBlocks
+﻿namespace Vitality.Website.Areas.Presales.ComponentTemplates.FeatureBlocks
 {
+    using System.Collections.Generic;
+
     public class VacancyListViewModel
     {
-        public string Headline { get; set; }
-        public string LocationsLabel { get; set; }
-        public string AllLocations { get; set; }
-        public string DepartmentsLabel { get; set; }
-        public string AllDepartments { get; set; }
-        public string FindOutMore { get; set; }
-        public string NoVacancies { get; set; }
-        public string VacancyLocation { get; set; }
-        public string VacancySalary { get; set; }
-        public string VacancyClosesOn { get; set; }
-        public string ApplyForVacancy { get; set; }
-        public string ShareForVacancy { get; set; }
-        public string BackToVacancyListing { get; set; }
-        public IEnumerable<string> Locations { get; set; }
+        public string AllDepartmentsText { get; set; }
+        public string AllLocationsText { get; set; }
+        public string ApplyForVacancyText { get; set; }
+        public string BackToVacanciesListText { get; set; }
         public IEnumerable<string> Departments { get; set; }
+        public string DepartmentsDropdownLabel { get; set; }
         public string FeedSettingsEndpoint { get; set; }
         public string FeedSettingsMockDataFileUrl { get; set; }
+        public string FindoutMoreText { get; set; }
+        public string Headline { get; set; }
+        public IEnumerable<string> Locations { get; set; }
+        public string LocationsDropdownLabel { get; set; }
+        public string LocationText { get; set; }
+        public string NoVacanciesFoundText { get; set; }
+        public string SalaryText { get; set; }
+        public string ShareVacancyText { get; set; }
+        public string VacancyClosesOn { get; set; }
     }
 }
