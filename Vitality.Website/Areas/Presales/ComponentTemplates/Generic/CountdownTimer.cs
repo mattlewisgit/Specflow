cfd894d921cbc76ea215802732d39d101464017b
Janaka Lakmal <janaka.lakmal@vitality.co.uk>
2017-05-02 16:04:22 +0100

    Countdowntimer added

---
+++ Vitality.Website/Areas/Presales/ComponentTemplates/Generic/CountdownTimer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using Vitality.Website.Areas.Global.Models;
+
+namespace Vitality.Website.Areas.Presales.ComponentTemplates.Generic
+{
+    public class CountdownTimer : SitecoreItem
+    {
+        public NameValueCollection Labels { get; set; }
+        public string RichText { get; set; }
+    }
+}
