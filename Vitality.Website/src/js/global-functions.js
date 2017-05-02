5108fd5b1c7e317ccba736d7e193fbde3b64aded
Janaka Lakmal <janaka.lakmal@vitality.co.uk>
2017-05-02 17:08:20 +0100

    Force add js files

---
+++ Vitality.Website/src/js/global-functions.js
@@ -0,0 +1,17 @@
+﻿// Memoize the regex creator to reuse the regex object.
+var _getURLParameterRegexCreator = _.memoize(function (name) {
+    "use strict";
+    return new RegExp("[?&]" + name + "=([^&]*)");
+});
+
+function getURLParameter(name, url) {
+    "use strict";
+
+    if (!name) {
+        return name;
+    }
+
+    url = url || window.location.search;
+    var match = _getURLParameterRegexCreator(name).exec(url);
+    return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+}
