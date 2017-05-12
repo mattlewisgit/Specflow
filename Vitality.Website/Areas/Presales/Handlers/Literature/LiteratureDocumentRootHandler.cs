50eea5991b58e4e5043d7505e0482f2b77b934ea
Damith Gunawardana <damith.gunawardanag@vitality.co.uk>
2017-05-12 17:42:44 +0100

    Updated csproj files to get the external assemblies. Lit library update to dispose the index context

--- Vitality.Website/Areas/Presales/Handlers/Literature/LiteratureDocumentRootHandler.cs
+++ Vitality.Website/Areas/Presales/Handlers/Literature/LiteratureDocumentRootHandler.cs
@@ -8,24 +8,25 @@ namespace Vitality.Website.Areas.Presales.Handlers.Literature
 {
     public class LiteratureDocumentRootHandler : IRequestHandler<LiteratureDocumentRequest, IEnumerable<LiteratureDocumentDto>>
     {
-        private readonly IProviderSearchContext _searchContext;
-
-        public LiteratureDocumentRootHandler()
+        public IEnumerable<LiteratureDocumentDto> Handle(LiteratureDocumentRequest request)
         {
+            IEnumerable<LiteratureDocumentDto> literatureDocuments = new List<LiteratureDocumentDto>();
+
             if (Sitecore.Context.Site != null)
             {
                 var searchableIndex = string.Format("{0}_literature_library", Sitecore.Context.Site.Name);
-                this._searchContext = ContentSearchManager.GetIndex(searchableIndex).CreateSearchContext();
-            }
-        }
 
-        public IEnumerable<LiteratureDocumentDto> Handle(LiteratureDocumentRequest request)
-        {
-            var searchResult = this._searchContext
-                .GetQueryable<LiteratureDocumentSearchResult>()
-                .Where(document => document.Library == request.Library);
-            
-            return LiteratureDocumentDto.From(searchResult);
+                using (var searchContext = ContentSearchManager.GetIndex(searchableIndex).CreateSearchContext())
+                {
+                    var searchResult = searchContext
+                        .GetQueryable<LiteratureDocumentSearchResult>()
+                        .Where(document => document.Library == request.Library);
+
+                    literatureDocuments = LiteratureDocumentDto.From(searchResult);
+                }
+            }
+
+            return literatureDocuments;
         }
     }
 }
