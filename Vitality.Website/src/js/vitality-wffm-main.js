8f41c547bf887cc038d514cef94b053f932e98b6
Darren Hickling <darren.hickling@vitality.co.uk>
2017-06-08 13:36:05 +0100

    DD-2470 BUG - Literature Library search issues Swapping out the WFFM loader script with a custom Vitality verison

---
+++ Vitality.Website/src/js/vitality-wffm-main.js
@@ -0,0 +1,9 @@
+﻿(function($) {
+    // This is a copy of the Sitecore WFFM main.js file,
+    // with one important difference: the omission of jQuery no conflict mode.
+    $(document).ready(function() {
+        $("form[data-wffm]").each(function() {
+            $(this).wffmForm();
+        });
+    });
+})(jQuery);
