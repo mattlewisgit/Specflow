25bac88e9fd43eff37c668f2f9e7a7c6af4c906c
DESKTOP-0RP5ML2\damith <damith.gunawardana@siprea.com>
2017-07-31 00:00:49 +0100

    Start coping changes to the feature branch

---
+++ Vitality.Website/Areas/Presales/Controllers/CallBackController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MediatR;
+using Vitality.Website.Areas.Presales.Models;
+
+namespace Vitality.Website.Areas.Presales.Controllers
+{
+    public class CallBackController : BaseController
+    {
+        public CallBackController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        [Route("api/callback")]
+        public HttpResponseMessage Post(CallBack model)
+        {
+            //Do The Validation
+
+
+        }
+
+
+    }
+}
---
+++ Vitality.Website/Areas/Presales/Models/CallBack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vitality.Website.Areas.Presales.Models
+{
+    public class CallBack
+    {
+        public string Title { get; set; }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string CallBackTime { get; set; }
+    }
+}
