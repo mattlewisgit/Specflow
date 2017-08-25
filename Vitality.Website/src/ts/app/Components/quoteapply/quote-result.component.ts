7f59a866c4b3660d065777f3fda5ad40dbb395d9
Janaka Lakmal <janaka.lakmal@vitality.co.uk>
2017-08-25 01:08:57 +0100

    enable editing , had to do a dirty trick before the review as componet Input doesn't get updated on complex objects.

--- Vitality.Website/src/ts/app/Components/quoteapply/quote-result.component.ts
+++ Vitality.Website/src/ts/app/Components/quoteapply/quote-result.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit }      from "@angular/core";
+import { Component, Input, OnInit}      from "@angular/core";
 import { WindowRef } from "./../windowref";
 import { Subscription } from "rxjs/Subscription";
 import { QuoteService } from "../../services/quote.service";
@@ -20,6 +20,10 @@ export class QuoteResultComponent implements OnInit {
 
     ngOnInit(): void {
         this.quoteResultData = this.winRef.nativeWindow.angularData.quoteResult;
+        this.getQuotes();
+    }
+
+    getQuotes(): void {
         this.quoteService.callRtpe(this.quoteResultData)
             .then((data: any) => {
                 this.quotes = data.Quotes;
@@ -32,5 +36,31 @@ export class QuoteResultComponent implements OnInit {
             return quote.PolicyGrossPremium;
         }
         return 0;
+    }
+
+    getBenefitOption(benefitOptions: BenefitOption[], permutationId:string): any {
+        return  benefitOptions.filter(x => x.permutations.filter(p => p === permutationId).length > 0)[0];
+    }
+    enableEdit(benefit: any): void {
+        benefit.isEditing = true;
+    }
+
+    optionChange(benefitId: string, benefitOptionCode: string): void {
+        this.quoteResultData.tickIcon = null;
+        const permutationIds = new Array<string>();
+        for (let permutation of this.quoteResultData.permutations) {
+            permutationIds.push(permutation.id);
+        }
+        for (let benefit of this.quoteResultData.benefits) {
+            if (benefit.id === benefitId) {
+                for (let benefitOption of benefit.benefitOptions) {
+                    benefitOption.permutations = [];
+                    if (benefitOptionCode === benefitOption.code) {
+                        benefitOption.permutations = permutationIds;
+                    }
+                }
+            }
+        }
+        this.getQuotes();
     }
 }
