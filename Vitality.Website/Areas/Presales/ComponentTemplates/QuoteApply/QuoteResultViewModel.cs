8d7cf687feaba9a7207e90067735344c02f43a11
Csaba Paronai <csaba.paronai@vitality.co.uk>
2017-09-14 14:32:43 +0100

    Adding pencil icon for editing a benefit + tooltip button text

--- Vitality.Website/Areas/Presales/ComponentTemplates/QuoteApply/QuoteResultViewModel.cs
+++ Vitality.Website/Areas/Presales/ComponentTemplates/QuoteApply/QuoteResultViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using Glass.Mapper.Sc.Configuration;
-using Glass.Mapper.Sc.Configuration.Attributes;
-using Vitality.Website.Areas.Global.Models;
+using Glass.Mapper.Sc.Fields;
 
 namespace Vitality.Website.Areas.Presales.ComponentTemplates.QuoteApply
 {
@@ -10,6 +8,7 @@ namespace Vitality.Website.Areas.Presales.ComponentTemplates.QuoteApply
         public IEnumerable<BenefitViewModel> Benefits { get; set; }
 
         public string CallToActionText { get; set; }
+        public Image EditIcon { get; set; }
         public string HelpText { get; set; }
 
         public IEnumerable<PermutationViewModel> Permutations { get; set; }
@@ -18,6 +17,7 @@ namespace Vitality.Website.Areas.Presales.ComponentTemplates.QuoteApply
         public string QuoteReferenceText { get; set; }
         public string QuoteValidText { get; set; }
         public string ServiceOutagePage { get; set; }
+        public string TooltipButtonText { get; set; }
         public string TooltipText { get; set; }
     }
 }
