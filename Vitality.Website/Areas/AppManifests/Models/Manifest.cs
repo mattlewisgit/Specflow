817c69946aba8dde7318dff8b2ea81a8d8959b0b
David Buckell <david.buckell@vitality.co.uk>
2017-09-14 22:03:26 +0100

    added app manifests

---
+++ Vitality.Website/Areas/AppManifests/Models/Manifest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vitality.Website.Areas.AppManifests.Models
+{
+    public class Manifest
+    {
+        /// <summary>
+        /// Gets or sets the manifest Data.
+        /// </summary>
+        public IList<Entity> Data { get; set; }
+    }
+}
