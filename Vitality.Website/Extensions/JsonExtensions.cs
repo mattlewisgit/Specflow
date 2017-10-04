4d62fb719321c568c412cc785dcc83a461f8202e
Janaka Lakmal <janaka.lakmal@vitality.co.uk>
2017-10-04 13:56:41 +0100

    use extention method for serilizing with camelcase serializer settings

---
+++ Vitality.Website/Extensions/JsonExtensions.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Vitality.Website.Areas.Global.Models;
+
+namespace Vitality.Website.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static string SerializeToCamelCase<T>(this SitecoreItem model)
+        {
+            return JsonConvert.SerializeObject(Mapper.Map<T>(model),
+                new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()});
+        }
+    }
+}
