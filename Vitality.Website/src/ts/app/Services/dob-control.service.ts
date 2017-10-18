349b8f5ff954e4abe96c75da28e9eff7d4c85264
Leandro Raveli <leandro.raveli@vitality.co.uk>
2017-10-18 15:32:27 +0100

    Chld DOB fix / Sitecore Sync

--- Vitality.Website/src/ts/app/Services/dob-control.service.ts
+++ Vitality.Website/src/ts/app/Services/dob-control.service.ts
@@ -97,6 +97,7 @@ export class DobControlService {
             childDobToAdd.placeholder = this.child1DobQuestion.placeholder;
             childDobToAdd.validators = this.child1DobQuestion.validators;
             childDobToAdd.controlType = this.child1DobQuestion.controlType;
+            childDobToAdd.relatedData = this.child1DobQuestion.relatedData;
             childDobToAdd.isHidden = true;
             this.childrenQuestionGroup.questions.push(childDobToAdd);
         }
